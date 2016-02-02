5b73332f7649411a77e585c43e26d2857222b060
Indira <indira_p_vemulapalli@optum.com>
2016-02-02 13:18:45 +0530

    F25540:US229548-Manage Drug List Estimated annual drug cost

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/AddDrugPage.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/AddDrugPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+  
 import acceptancetests.atdd.util.CommonUtility;
 import atdd.framework.UhcDriver;
 
