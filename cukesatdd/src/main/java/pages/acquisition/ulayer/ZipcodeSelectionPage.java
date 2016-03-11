ce68dad70722697dd49164d14ad2ec0bbea3e3ce
rpaul103 <rubina.paul@optum.com>
2016-03-11 10:10:26 -0500

    F25558:US231540- Automation changes code clean up -updating zipselection unique id which was pre-exisiting

--- cukesatdd/src/main/java/pages/acquisition/ulayer/ZipcodeSelectionPage.java
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/ZipcodeSelectionPage.java
@@ -20,7 +20,7 @@ import atdd.framework.UhcDriver;
  */
 public class ZipcodeSelectionPage extends UhcDriver {
 
-	@FindBy(id = "selectcountytable")
+	@FindBy(id = "confirmZipCodeTable")
 	private WebElement ZipcodeTable;
 
 	private PageData zipSelection;
