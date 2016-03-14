f418c13db34f25b8278689dd5cf469e867028cbf
rpaul103 <rubina.paul@optum.com>
2016-03-14 09:28:47 -0400

    F20882:US233968-Acquisition Optimization - Home Page UI - Zip Code Lookup

---
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/ZipcodeSelectionHomePage.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package pages.acquisition.bluelayer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import acceptancetests.atdd.data.CommonConstants;
+import acceptancetests.atdd.data.PageData;
+import acceptancetests.atdd.util.CommonUtility;
+import atdd.framework.UhcDriver;
+
+/**
+ * @author pperugu
+ *
+ */
+public class ZipcodeSelectionHomePage extends UhcDriver {
+
+	@FindBy(id = "selectcountytable")
+	private WebElement ZipcodeTable;
+
+	private PageData zipSelectionhome;
+
+	public JSONObject zipSelectionhomeJson;
+
+	public ZipcodeSelectionHomePage(WebDriver driver) {
+		super(driver);
+		String fileName = CommonConstants.ZIPCODE_SELECTION_HOME_PAGE_DATA;
+		zipSelectionhome = CommonUtility.readPageData(fileName,
+				CommonConstants.PAGE_OBJECT_DIRECTORY_BLUELAYER_ACQ);
+		openAndValidate();
+	}
+
+	@Override
+	public void openAndValidate() {
+
+		JSONObject jsonObject = new JSONObject();
+		for (String key : zipSelectionhome.getExpectedData().keySet()) {
+			WebElement element = findElement(zipSelectionhome.getExpectedData()
+					.get(key));
+			if (null != element) {
+				if (validate(element)) {
+					try {
+						jsonObject.put(key, element.getText());
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		zipSelectionhomeJson = jsonObject;
+	}
+}
