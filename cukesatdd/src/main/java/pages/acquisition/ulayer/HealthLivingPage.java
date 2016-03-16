7970454cf46ac77108d21519141b47bad765906b
rpaul103 <rubina.paul@optum.com>
2016-03-16 12:49:44 -0400

    F20882:US231540- Code fix after merge issues

---
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/HealthLivingPage.java
@@ -0,0 +1,73 @@
+package pages.acquisition.ulayer;
+
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import acceptancetests.login.data.LoginCommonConstants;
+
+/**
+ * @author naggarw2
+ *
+ */
+public class HealthLivingPage extends GlobalWebElements{
+		
+
+	public HealthLivingPage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+		openAndValidate();
+	}
+	
+	
+
+		@FindBy(xpath = "//*[@id='ghn_lnk_4']")
+		private WebElement hoverhealthandwellnesslink;
+	
+	
+		@FindBy(xpath = "//*[@id='subnav_4']/div/div/div[1]/div[1]/div[2]/p[2]/a/span")
+		private WebElement healthlivinglink;
+	
+	
+	
+public void hoverhealthandwellnesslink() {
+	
+		
+		validate(hoverhealthandwellnesslink);
+			//Hover over text
+			Actions action = new Actions(driver);
+			action.moveToElement(hoverhealthandwellnesslink).build().perform();
+			
+			//validate(hoverhealthandwellnesslink);
+			
+			// TODO Auto-generated method stub
+		
+		}
+
+
+	public HealthToolsPage HealthToolsClick() {
+		
+		driver.navigate().back();
+		hoverhealthandwellnesslink();
+		validate(healthlivinglink);
+		Actions actions = new Actions(driver);
+		 actions.moveToElement(healthlivinglink);
+	      actions.click().build().perform();
+		
+	      if(driver.getTitle().equalsIgnoreCase("Health Tools")){
+		    	return new HealthToolsPage(driver);
+	}
+	     
+
+	// TODO Auto-generated method stub
+	return null;
+}
+
+		
+	
+}
