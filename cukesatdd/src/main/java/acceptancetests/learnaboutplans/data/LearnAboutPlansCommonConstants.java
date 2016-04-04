edf4cf5b55a428c96184d9199957a38d23b610f4
h1000 <himachal@optum.com>
2016-04-04 23:39:08 +0530

    F20882:US231540-Acquisition Optimization - Home Page

---
+++ cukesatdd/src/main/java/acceptancetests/learnaboutplans/data/LearnAboutPlansCommonConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package acceptancetests.learnaboutplans.data;
+
+/**
+ * @author pperugu
+ *
+ */
+public class LearnAboutPlansCommonConstants {
+	public static final String LEARN_ABOUT_PLANS_EXPECTED = "learnaboutplansExpectedJson";
+	public static final String LEARN_ABOUT_PLANS_ACTUAL = "learnaboutplansActualJson";
+	public static final String LEARN_ABOUT_PLANS = "learnaboutplans";
+}
