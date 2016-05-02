2b7157e402e014ea813536f18e78fdf5d0089c6b
pnampall <pinki_nampally@optum.com>
2016-05-02 17:05:49 +0530

    F32044:US265759-Added MRAttd Script for mobile pages

--- cukesatdd/src/main/java/acceptancetests/atdd/data/MRConstants.java
+++ cukesatdd/src/main/java/acceptancetests/atdd/data/MRConstants.java
@@ -37,6 +37,8 @@ public class MRConstants {
 	
 	public static final String AARPM_MOBILE_URL = "https://member.awe-"+MRScenario.environment+"-aarpmedicareplans.uhc.com/mobile/guest/login.html";
 	
+	public static final String UHCM_MOBILE_URL = "https://member.awe-"+MRScenario.environment+"-uhcmedicaresolutions.uhc.com/mobile/guest/login.html";
+	
 	public static final String MRREST_TIME_ADMIN_URL = "https://awe-"+MRScenario.environment+"-generic.uhc.com/MRRestWAR/rest/zadmin/time/";
 	
 	public static final String PARTD_TIME_ADMIN_URL = "https://awe-"+MRScenario.environment+"-generic.uhc.com/PartDPortalWeb/rest/zadmin/time/";
--- cukesatdd/src/main/java/acceptancetests/atdd/mobile/data/CommonConstants.java
+++ cukesatdd/src/main/java/acceptancetests/atdd/mobile/data/CommonConstants.java
@@ -12,9 +12,26 @@ public class CommonConstants {
 	public static final String WEBDRIVER = "webDriver";
 	
 	public static final String BENEFITS_SUMMARY_PAGE_DATA = "benefitssummary.json"; 
+	
+	public static final String BENEFITS_SUMMARY_PAGE_DATA_BLUE_LAYER = "benefitssummary.json"; 
 
 	public static final String PAGE_OBJECT_DIRECTORY_MOBILE_ULAYER_MEMBER = "/page-objects/mobile/member/ulayer/";
 
+	public static final String PAGE_OBJECT_DIRECTORY_MOBILE_BLUELAYER_MEMBER = "/page-objects/mobile/member/bluelayer/";
+	
 	public static final String EXPECTED_DATA_MAP = "expectedDataMap";
+	
+	public static final String MOBILE_BROWSER_CHECK_DATA="browsercheck.json";
+	
+	public static final String MOBILE_BROWSER_CHECK_DATA_BLUELAYER="browsercheck_uhcm.json";
+	
+	public static final String MOBILE_EXPECTED_DIRECTORY = "/jsonresponse/mobile/member";
+	
+	public static final String SITE_ULAYER_MEMBER="ulayer";
+	
+	public static final String SITE_BLUELAYER_MEMBER="bluelayer";
+	
+	
+	
 
 }
