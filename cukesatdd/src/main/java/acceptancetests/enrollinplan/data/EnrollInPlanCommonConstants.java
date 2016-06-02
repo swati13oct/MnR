b908ce9f9fdac20672a979b63c915e20cb3005d9
rpaul103 <rubina.paul@optum.com>
2016-06-02 11:28:35 -0400

    F25552:US288363- Technical Story: Implement Automation

--- cukesatdd/src/main/java/acceptancetests/enrollinplan/data/EnrollInPlanCommonConstants.java
+++ cukesatdd/src/main/java/acceptancetests/enrollinplan/data/EnrollInPlanCommonConstants.java
@@ -9,11 +9,14 @@ package acceptancetests.enrollinplan.data;
  */
 public class EnrollInPlanCommonConstants {
 
+	public static final String COMMON_DATA = "common";
+	public static final String PLAN_NAME = "planName";
 	public static final String ENROLL_PLAN_INFO = "enrollplaninfo";
 	public static final String ENROLL_IN_PLAN_FLOW_NAME = "enrollinplan";
 	public static final String PLAN_INFORMATION = "planinformation";
 	public static final String INTRODUCTION_INFORMATION = "introductioninformation";
 	public static final String BENEFICIARY_INFORMATION = "beneficiaryinformation";
+	public static final String SEP_INFORMATION = "sepinformation";
 	public static final String ESRD_INFORMATION = "esrdinformation";
 	public static final String PDC_INFORMATION = "prescriptiondrugcoverageinformation";
 	public static final String LTC_INFORMATION = "longtermcareinformation";
@@ -22,16 +25,12 @@ public class EnrollInPlanCommonConstants {
 	public static final String PCP_INFORMATION = "primarycareproviderinformation";
 	public static final String OPT_INFORMATION = "optionalridersinformation";
 	public static final String PPO_INFORMATION = "planpaymentinformation";
-	public static final String COMMON_DATA = "common";
-	public static final String PLAN_NAME = "planName";
-	public static final String ADDITIONAL_INFORMATION = "additionalinformation";
+	public static final String PED = "proposedEffectiveDate";
 	public static final String REVIEW_APPLICATION = "reviewapplication";
 	public static final String SUBMIT_APPLICATION = "submitapplication";
 	public static final String ENROLLMENT_CONFIRMATION = "enrollmentconfirmation";
 	public static final String ENROLLMENT_CONFIRMATION_ACTUAL = "enrollmentConfirmationActual";
 	public static final String ENROLLMENT_CONFIRMATION_EXPECTED = "enrollmentConfirmationExpected";
-	public static final String SEP_INFORMATION = "sepinformation";
-	public static final String PED = "proposedEffectiveDate";
-	
 
+	public static final String ADDITIONAL_INFORMATION = "additionalinformation";
 }
