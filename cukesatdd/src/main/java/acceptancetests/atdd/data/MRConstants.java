a6d4a767e8f4c7799d48fdc64adbdd5c08aa7c01
pemmadi <purushotham_emmadi@optum.com>
2016-09-29 14:19:22 +0530

    F48888:US375418-South Florida PCP front door

--- cukesatdd/src/main/java/acceptancetests/atdd/data/MRConstants.java
+++ cukesatdd/src/main/java/acceptancetests/atdd/data/MRConstants.java
@@ -80,6 +80,10 @@ public class MRConstants {
 //public static final String UHC_URL = "https://www.awe-"+MRScenario.environment+"-uhcmedicaresolutions.uhc.com/";
 	public static final String UHC_URL = "https://www."+MRScenario.environment+"-uhcmedicaresolutions.uhc.com/";
 	
+	public static final String PCP_PAGE_URL = "https://www.awe-"+MRScenario.environment+"-mypcpmedicare.uhc.com/";
+	
+	public static final String MEDICA_PAGE_URL = "https://www.awe-"+MRScenario.environment+"-mymedicamedicare.uhc.com/";
+	
 	public static final String AARPM_REGISTRATION_URL = "https://member.awe-"+MRScenario.environment+"-aarpmedicareplans.uhc.com/guest/registration.html";
 	
 	public static final String UHCM_REGISTRATION_URL = "https://member.awe-"+MRScenario.environment+"-uhcmedicaresolutions.uhc.com/guest/registration.html";
---
+++ cukesatdd/src/main/java/atdd/runners/RunAtddTestMyPcp.java
@@ -0,0 +1,14 @@
+package atdd.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.mypcp" }, 
+		features = { "feature/mypcp" }, 
+		format = {
+		"pretty", "html:reports/test-report" }, tags ={"@mypcp"})
+public class RunAtddTestMyPcp {
+
+}
