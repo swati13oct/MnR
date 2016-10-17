7d4cd39e91acd23df8ecda411fb5ea9b66afd0e7
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2016-10-17 15:12:23 +0530

    F51515:US374078-AL PEEHIP PHARMACY LOCATOR SEARCH ENGLISH MOBILE

--- cukesatdd/src/main/java/pages/mobile/member/blayer/BenefitsSummaryPage.java
+++ cukesatdd/src/main/java/pages/mobile/member/blayer/BenefitsSummaryPage.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -23,7 +24,7 @@ import atdd.framework.UhcDriver;
  * @author pnampall
  *
  */
-public class BenefitsSummaryPage extends UhcDriver {
+public class BenefitsSummaryPage extends PharmacyLocator {
 	
 	@FindBy(xpath="//div[@class='site-header']/a")
 	private WebElement menuButton;
@@ -33,6 +34,12 @@ public class BenefitsSummaryPage extends UhcDriver {
 	
 	@FindBy(className = "menu-container")
 	private WebElement menuContainer;
+	
+	@FindBy(xpath="/html/body/div[2]/a")
+	private WebElement menu;
+	
+	@FindBy(xpath="//div[2]/a[3]")
+	private WebElement pharamcyLocatorLink;
 	
 	private PageData benefitsSummary;
 
@@ -117,6 +124,29 @@ public class BenefitsSummaryPage extends UhcDriver {
 		}
 	}
 
+	 public void navigateToPharamcyPage(){
+		menu.click();
+		pharamcyLocatorLink.click();
+		
+	 }
+	 
+	 public void validatePharmacyPage(){
+		 filterButton.click();
+		 if(mailServiceRadioCheckbox.isDisplayed()){
+			 System.out.println("---------------Failed due to presence of mail service checkbox-------");
+	         Assert.fail();	 
+		 }
+		 if(mailServiceRadioText.isDisplayed()){
+			 System.out.println("--------------Failed due to presence of mail service tet--------------");
+			 Assert.fail();
+		 }
+		 toolTip.click();
+		 if(toolTipMailServiceContent.isDisplayed()){
+			 System.out.println("--------------Failed due to presence of mail service in tool tip-----------------");
+			 Assert.fail();
+		 }
+		 
+	 }
 	public JSONObject getBrowserCheck() {
 	
 		String fileName = CommonConstants.MOBILE_BROWSER_CHECK_DATA_BLUELAYER;
