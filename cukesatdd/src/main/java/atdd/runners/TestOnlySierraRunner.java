b700baf2fda0fb5de60d6db78888b80198648330
sunya <sumit_unya@optum.com>
2016-10-27 17:46:31 +0530

    F51250:US360430- deleting testonlyrunner

--- cukesatdd/src/main/java/atdd/runners/TestOnlySierraRunner.java
+++ cukesatdd/src/main/java/atdd/runners/TestOnlySierraRunner.java
@@ -8,14 +8,14 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 /**	
- * @author ganilku1
+ * @author sunya
  *
  */
 @RunWith(Cucumber.class)	
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.testonly" }, 
-		features = { "feature/testonly" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.testonly.blayer" }, 
+		features = { "feature/testonly/blayer" }, 
 		format = {
-		"pretty", "html:reports/test-report" }, tags ={"@testonly-seirra"})
+		"pretty", "html:reports/test-report" }, tags ={"@testonly-seirra003"})
 public class TestOnlySierraRunner {
 	
 }
