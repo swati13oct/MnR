5f7625ab5b8f445248b2527c4481029f3d61ad02
kchaudh3 <kshitij_chaudhary@optum.com>
2016-10-31 16:30:49 +0530

    F48875:US395950-South Florida Medica members able to register to site My Account Home page

--- cukesatdd/src/main/java/pages/mymedica/RegistrationSuccessPage.java
+++ cukesatdd/src/main/java/pages/mymedica/RegistrationSuccessPage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import pages.member.bluelayer.AccountHomePage;
 import acceptancetests.atdd.data.CommonConstants;
 import acceptancetests.atdd.data.PageData;
 import acceptancetests.atdd.util.CommonUtility;
