6ccdfdf119c92201bb17aaeadc5f59443ad3d0bd
Nidhi <nidhi_mishra@optum.com>
2016-12-24 03:47:16 +0530

    F64313:US469206-Commiting changes

--- cukesatdd/src/main/java/acceptancetests/mobile/plandetail/ulayer/PlanDetailStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/mobile/plandetail/ulayer/PlanDetailStepDefinition.java
@@ -354,4 +354,260 @@ getLoginScenario().saveBean(CommonConstants.EXPECTED_DATA_MAP, expectedDataMap);
 		}
 		getLoginScenario().saveBean(CommonConstants.EXPECTED_DATA_MAP, expectedDataMap);
 	}
+	
+	@When("^the user logs in with a registered Ship member with following details in AARP site$")
+	public void user_log_in(DataTable memberAttributes) {
+		/* Reading the given attribute from feature file */
+		List<DataTableRow> memberAttributesRow = memberAttributes
+				.getGherkinRows();
+		Map<String, String> memberAttributesMap = new LinkedHashMap<String, String>();
+		for (int i = 0; i < memberAttributesRow.size(); i++) {
+
+			memberAttributesMap.put(memberAttributesRow.get(i).getCells()
+					.get(0), memberAttributesRow.get(i).getCells().get(1));
+		}
+
+		Set<String> memberAttributesKeySet = memberAttributesMap.keySet();
+		List<String> desiredAttributes = new ArrayList<String>();
+		for (Iterator<String> iterator = memberAttributesKeySet.iterator(); iterator
+				.hasNext();) {
+			{
+				String key = iterator.next();
+				desiredAttributes.add(memberAttributesMap.get(key));
+			}
+
+		}
+		desiredAttributes.add("mobile");
+		System.out.println("desiredAttributes.." + desiredAttributes);
+
+		Map<String, String> loginCreds = loginScenario
+				.getAMPMemberWithDesiredAttributes(desiredAttributes);
+
+		String userName = null;
+		String pwd = null;
+		if (loginCreds == null) {
+			// no match found
+			System.out.println("Member Type data could not be setup !!!");
+			Assert.fail("unable to find a " + desiredAttributes + " member");
+		} else {
+			userName = loginCreds.get("user");
+			pwd = loginCreds.get("pwd");
+			System.out.println("User is..." + userName);
+			System.out.println("Password is..." + pwd);
+			getLoginScenario()
+					.saveBean(LoginCommonConstants.USERNAME, userName);
+			getLoginScenario().saveBean(LoginCommonConstants.PASSWORD, pwd);
+		}
+
+		LoginPage loginPage = (LoginPage) getLoginScenario().getBean(
+				PageConstants.LOGIN_PAGE);
+
+		BenefitsSummaryPage benefitsSummaryPage = loginPage.loginWith(userName,
+				pwd);
+
+		/* Get expected data */
+		Map<String, JSONObject> expectedDataMap = loginScenario
+				.getExpectedJson(userName);
+		/*
+		 * JSONObject benefitsSummaryExpectedJson =
+		 * benefitsSummaryPage.getExpectedData(expectedDataMap);
+		 * getLoginScenario
+		 * ().saveBean(LoginCommonConstants.BENEFITS_SUMMARY_EXPECTED,
+		 * benefitsSummaryExpectedJson);
+		 * 
+		 * JSONObject benefitsSummaryActualJson = null;
+		 */
+		if (benefitsSummaryPage != null) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			getLoginScenario().saveBean(PageConstants.BENEFITS_SUMMARY_PAGE,
+					benefitsSummaryPage);
+			/*
+			 * benefitsSummaryActualJson =
+			 * benefitsSummaryPage.benefitsSummaryJson;
+			 * getLoginScenario().saveBean
+			 * (LoginCommonConstants.BENEFITS_SUMMARY_ACTUAL,
+			 * benefitsSummaryActualJson);
+			 */}
+
+		getLoginScenario().saveBean(CommonConstants.EXPECTED_DATA_MAP,
+				expectedDataMap);
+
+	}
+
+	@And("^user clicks on view detials button Benefit Page and clicks on hospital link$")
+	public void click_viewdetails() {
+		BenefitsSummaryPage benefitsSummaryPage = (BenefitsSummaryPage) getLoginScenario()
+				.getBean(PageConstants.BENEFITS_SUMMARY_PAGE);
+
+		BenefitDetailsPage benefitsDetailPage = benefitsSummaryPage.viewdetailsclick();
+		
+		if (benefitsDetailPage != null) {
+			getLoginScenario().saveBean(PageConstants.BENEFITS_DETAIL_PAGE,
+					benefitsDetailPage);
+			Assert.assertTrue(true);
+		} else {
+			Assert.fail(" Page not found");
+		}
+	}
+
+	@And("^user view hospital benefit$")
+	public void user_navigates_to_detail_page()
+
+	{
+
+		BenefitDetailsPage benefitsDetailPage = (BenefitDetailsPage) getLoginScenario()
+				.getBean(PageConstants.BENEFITS_DETAIL_PAGE);
+
+		//benefitsDetailPage.hospitalstayclick();
+		System.out.println("username is"+getLoginScenario().getBean(
+				LoginCommonConstants.USERNAME).toString());
+		Map<String, JSONObject> expectedDataMap = loginScenario
+				.getExpectedJson(getLoginScenario().getBean(
+						LoginCommonConstants.USERNAME).toString());
+		JSONObject benefitsDetailExpectedJson = benefitsDetailPage
+				.getExpectedDataship(expectedDataMap);
+		getLoginScenario().saveBean(
+				LoginCommonConstants.BENEFITS_DETAIL_EXPECTED,
+				benefitsDetailExpectedJson);
+		System.out.println("benefitsDetailExpectedJson"+benefitsDetailExpectedJson);
+
+		JSONObject benefitsDetailActualJson = null;
+		if (benefitsDetailPage != null) {
+			getLoginScenario().saveBean(PageConstants.BENEFITS_DETAIL_PAGE,
+					benefitsDetailPage);
+			benefitsDetailActualJson = benefitsDetailPage.benefitsDetailJson;
+			getLoginScenario().saveBean(
+					LoginCommonConstants.BENEFITS_DETAIL_ACTUAL,
+					benefitsDetailActualJson);
+		}
+		System.out.println("benefitsDetailActualJson"+benefitsDetailActualJson);
+
+		getLoginScenario().saveBean(CommonConstants.EXPECTED_DATA_MAP,
+				expectedDataMap);
+		
+		try {
+			JSONAssert.assertEquals(benefitsDetailExpectedJson, benefitsDetailActualJson, true);
+			} catch (JSONException e) {
+			e.printStackTrace();
+			}
+
+	}
+	
+	@And("^user clicks on view detials button Benefit Page for medical care and clicks on medical care link$")
+	public void click_viewdetailsmedicalcare() {
+		BenefitsSummaryPage benefitsSummaryPage = (BenefitsSummaryPage) getLoginScenario()
+				.getBean(PageConstants.BENEFITS_SUMMARY_PAGE);
+
+		BenefitDetailsPage benefitsDetailPage = benefitsSummaryPage.viewdetailsmedicalcareclick();
+		
+		if (benefitsDetailPage != null) {
+			getLoginScenario().saveBean(PageConstants.BENEFITS_DETAIL_PAGE,
+					benefitsDetailPage);
+			Assert.assertTrue(true);
+		} else {
+			Assert.fail(" Page not found");
+		}
+	}
+
+	@And("^the user view medical care benefit$")
+	
+	public void user_navigates_to_medicalcarebenefitpage()
+	{
+	BenefitDetailsPage benefitsDetailPage = (BenefitDetailsPage) getLoginScenario()
+	.getBean(PageConstants.BENEFITS_DETAIL_PAGE);
+
+//benefitsDetailPage.hospitalstayclick();
+System.out.println("username is"+getLoginScenario().getBean(
+	LoginCommonConstants.USERNAME).toString());
+Map<String, JSONObject> expectedDataMap = loginScenario
+	.getExpectedJson(getLoginScenario().getBean(
+			LoginCommonConstants.USERNAME).toString());
+JSONObject benefitsDetailExpectedJson = benefitsDetailPage
+	.getExpectedDataship(expectedDataMap);
+getLoginScenario().saveBean(
+	LoginCommonConstants.BENEFITS_DETAIL_EXPECTED,
+	benefitsDetailExpectedJson);
+System.out.println("benefitsDetailExpectedJson"+benefitsDetailExpectedJson);
+
+JSONObject benefitsDetailActualJson = null;
+if (benefitsDetailPage != null) {
+getLoginScenario().saveBean(PageConstants.BENEFITS_DETAIL_PAGE,
+		benefitsDetailPage);
+benefitsDetailActualJson = benefitsDetailPage.benefitsDetailJson;
+getLoginScenario().saveBean(
+		LoginCommonConstants.BENEFITS_DETAIL_ACTUAL,
+		benefitsDetailActualJson);
 }
+System.out.println("benefitsDetailActualJson"+benefitsDetailActualJson);
+
+getLoginScenario().saveBean(CommonConstants.EXPECTED_DATA_MAP,
+	expectedDataMap);
+
+try {
+JSONAssert.assertEquals(benefitsDetailExpectedJson, benefitsDetailActualJson, true);
+} catch (JSONException e) {
+e.printStackTrace();
+}
+	}
+
+
+@And("^view skilled facility benefit$")
+
+public void user_navigates_to_skilledfacilityclick()
+
+{
+
+	BenefitDetailsPage benefitsDetailPage = (BenefitDetailsPage) getLoginScenario()
+.getBean(PageConstants.BENEFITS_DETAIL_PAGE);
+
+Map<String,JSONObject> expectedDataMap = loginScenario.getExpectedJson(getLoginScenario().getBean(LoginCommonConstants.USERNAME).toString());
+JSONObject benefitsDetailExpectedJson = benefitsDetailPage.getExpectedDataship(expectedDataMap);
+getLoginScenario().saveBean(LoginCommonConstants.BENEFITS_DETAIL_EXPECTED, benefitsDetailExpectedJson);
+System.out.println("benefitsDetailExpectedJson"+benefitsDetailExpectedJson);
+
+JSONObject benefitsDetailActualJson =  null;
+if (benefitsDetailPage != null) {
+	getLoginScenario().saveBean(PageConstants.BENEFITS_DETAIL_PAGE, benefitsDetailPage);
+	benefitsDetailActualJson = benefitsDetailPage.benefitsDetailJson;
+	getLoginScenario().saveBean(LoginCommonConstants.BENEFITS_DETAIL_ACTUAL, benefitsDetailActualJson);
+}
+System.out.println("benefitsDetailActualJson"+benefitsDetailActualJson);
+
+getLoginScenario().saveBean(CommonConstants.EXPECTED_DATA_MAP, expectedDataMap);
+
+try
+{
+	if(benefitsDetailExpectedJson.equals(benefitsDetailActualJson))
+	{
+		Assert.assertTrue(true);
+	}
+	}
+catch(Exception e)
+{
+	 e.printStackTrace();
+}
+}
+
+@And("^user clicks on view detials button and click on skilled nursing benefit$")
+public void click_viewdetailsskilled() {
+	BenefitsSummaryPage benefitsSummaryPage = (BenefitsSummaryPage) getLoginScenario()
+			.getBean(PageConstants.BENEFITS_SUMMARY_PAGE);
+
+	BenefitDetailsPage benefitsDetailPage = benefitsSummaryPage.viewdetailsskilledclick();
+	
+	if (benefitsDetailPage != null) {
+		getLoginScenario().saveBean(PageConstants.BENEFITS_DETAIL_PAGE,
+				benefitsDetailPage);
+		Assert.assertTrue(true);
+	} else {
+		Assert.fail(" Page not found");
+	}
+}
+
+}
+
