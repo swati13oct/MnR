289764ab628bed5d7f480a8113a89046658ee078
mrani101 <rani_madadi@optum.com>
2017-02-13 17:03:29 +0530

    F71424:US463116-ATDD code changes for mercury project

--- cukesatdd/src/main/java/pages/member/bluelayer/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/member/bluelayer/AccountHomePage.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import pages.member.bluelayer.OrderplanmaterialsPage;
 import pages.member.bluelayer.ContactUsPage;
--- cukesatdd/src/main/java/pages/member/ulayer/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/member/ulayer/AccountHomePage.java
@@ -10,12 +10,14 @@ import java.util.Map;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import acceptancetests.atdd.data.CommonConstants;
 import acceptancetests.atdd.data.PageData;
