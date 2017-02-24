650a5a8261e0b9791e6457404740b063b2681ffb
Venkatesh <venkatesh_k_nalla@optum.com>
2017-02-24 16:27:21 +0530

    F0000:US518211-merge conflicts to resolving

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddPaymentsTest.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddPaymentsTest.java
@@ -11,11 +11,11 @@ import cucumber.junit.Cucumber;
  * @author pperugu
  *
  */
-@RunWith(Cucumber.class)
+/*@RunWith(Cucumber.class)
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.payments.ulayer" }, 
 		features = { "feature/payments/OneTimePayment/ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report" }, tags ={"@payments"})
+		"pretty", "html:reports/test-report" }, tags ={"@payments"})*/
 
 
 
