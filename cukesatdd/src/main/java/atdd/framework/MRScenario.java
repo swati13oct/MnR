710fb5abb5353508010c0899ecfcb901cf3fff6c
John Antolak <john.antolak@optum.com>
2017-02-28 12:54:02 -0600

    F00000:US527729-Check in a working example so that we can debug PhantomJS browser 2017-02-28 PM

--- cukesatdd/src/main/java/atdd/framework/MRScenario.java
+++ cukesatdd/src/main/java/atdd/framework/MRScenario.java
@@ -930,7 +930,7 @@ public class MRScenario {
 				//end from jarvis
 				webDriver = new PhantomJSDriver(caps);
 				webDriver.manage().window().setSize(new Dimension(1400,1000));
-				webDriver.manage().timeouts().pageLoadTimeout(60,TimeUnit.SECONDS);
+				webDriver.manage().timeouts().pageLoadTimeout(120,TimeUnit.SECONDS);
 			} else if (browser.equalsIgnoreCase(CommonConstants.FIREFOX_BROWSER)) {
 				FirefoxBinary ffBinary = new FirefoxBinary(new File(pathToBinary));
 				FirefoxProfile firefoxProfile = new FirefoxProfile();
