260c46147f8fd77ad79d6fbf9e19c50387eb8bb4
John Antolak <john.antolak@optum.com>
2017-03-08 07:20:08 -0600

    F00000:US539951-Jenkin automated test - Revert running to use available rider test.

--- cukesatdd/src/main/java/atdd/runners/AntolakRunTestChat.java
+++ cukesatdd/src/main/java/atdd/runners/AntolakRunTestChat.java
@@ -15,7 +15,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoverage" }, 
 		features = { "feature/debug" }, 
 		format = {
-		"pretty", "html:reports/test-report" }, tags ={"@jantolakTest, @replaceRider"})
+		"pretty", "html:reports/test-report" }, tags ={"@availableRiders"})
 public class AntolakRunTestChat {
 
 }
