5a840debe1b39e25ce52a07a4e56f2561b673eeb
mrani101 <rani_madadi@optum.com>
2017-03-17 20:21:14 +0530

    F70838:US459527-ATDD code changes for Sprint 4

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestPharmacylocator.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestPharmacylocator.java
@@ -15,7 +15,8 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.pharmacylocator.member.ulayer" }, 
 		features = { "feature/pharmacy-locator/member/ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report" }, tags ={"@pharmacysearch"})
+		"pretty", "html:reports/test-report", "json:target/cucumber.json" }, tags ={"@pharmacysearch"})
+
 public class RunMRAtddTestPharmacylocator {
 
 }
