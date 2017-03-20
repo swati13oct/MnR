524d5462176e80014b15b42a3706225d2d6dae2c
Vaishnavi <krishna_senapathi@optum.com>
2017-03-20 15:30:35 +0530

    F51845:US554322-JenkinsSetup

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestClaimsRedesign.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestClaimsRedesign.java
@@ -14,7 +14,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.dashboard.claims.bluelayer" }, 
 features = { "feature/dashboard/claims/bluelayer" }, 
 format = {
-		"pretty", "html:reports/test-report" }, tags ={"@claimsSample"})
+		"pretty", "html:reports/test-report", "json:target/cucumber.json" }, tags ={"@claimsSample"})
 public class RunMRAtddTestClaimsRedesign {
 
 }
