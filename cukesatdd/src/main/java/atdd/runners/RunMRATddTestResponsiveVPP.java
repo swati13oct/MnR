9096334184d9dda738d98958b587f1fb394fcbd1
wzhong <weixin.zhong@optum.com>
2017-03-30 13:27:17 -0500

    F32132:US507226-April release sprint 3

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
@@ -8,7 +8,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.ulayer" }, 
 		features = { "feature/responsive/Ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@aprilReleaseSprint2"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@aprilReleaseSprint3"})
 public class RunMRATddTestResponsiveVPP {
 
 }
