6753e645a01de2d8335d32123ad2d2507bad6bf8
njain112 <nikita_jain@optum.com>
2017-03-31 13:42:54 +0530

    F83993:US506366-Group Need Help contact

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBnC.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBnC.java
@@ -12,10 +12,10 @@ import cucumber.junit.Cucumber;
  *
  */
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoverage.bluelayer" }, 
-		features = { "feature/plan-benefits-and-coverage/bluelayer" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoveragejenkins.ulayer" }, 
+		features = { "feature/plan-benefits-and-coverage/ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@needHelpums"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@activeRider3"})
 public class RunMRAtddTestBnC {
 
 }
