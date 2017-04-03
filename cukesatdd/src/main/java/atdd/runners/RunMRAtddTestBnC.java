9c95212c71175fc3904d9fbdd1974087d04573ea
njain112 <nikita_jain@optum.com>
2017-04-03 15:00:36 +0530

    F83993:US506366-contact us

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBnC.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBnC.java
@@ -15,7 +15,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoveragejenkins.ulayer" }, 
 		features = { "feature/plan-benefits-and-coverage/ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@activeRider3"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@availableRider3"})
 public class RunMRAtddTestBnC {
 
 }
