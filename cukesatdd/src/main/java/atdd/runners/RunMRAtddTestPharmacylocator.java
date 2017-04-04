44b1dccdcfccfc129e0821c911c642d2fb8ffb67
mrani101 <rani_madadi@optum.com>
2017-04-04 18:03:04 +0530

    F70838:US459522-ATDD code changes for April release

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestPharmacylocator.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestPharmacylocator.java
@@ -15,7 +15,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.pharmacylocator.member.bluelayer" }, 
 		features = { "feature/pharmacy-locator/member/bluelayer" }, 
 		format = {
-		"pretty", "html:reports/test-report", "json:target/cucumber.json" }, tags ={"@US459522,@US459888,@texasers,@bluePharmacypreferedwidget,@pharmacyfilterandtooltip"})
+		"pretty", "html:reports/test-report", "json:target/cucumber.json" }, tags ={"@pharmacyfilterandtooltip"})
 
 public class RunMRAtddTestPharmacylocator {
 
