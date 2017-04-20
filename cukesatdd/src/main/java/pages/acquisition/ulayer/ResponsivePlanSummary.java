389c4d81d12021dc5f02cea78088175080787780
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-04-20 13:36:52 +0530

    F00000:US000000-Wait Statement

--- cukesatdd/src/main/java/pages/acquisition/ulayer/ResponsivePlanSummary.java
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/ResponsivePlanSummary.java
@@ -577,6 +577,12 @@ public void comparePlanslnk(){
 						e.printStackTrace();
 					}						  
 							ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+							try {
+								Thread.sleep(6000);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}		
 							driver.switchTo().window(tabs.get(1));
 							System.out.println(driver.getTitle());
 							if (driver.getTitle().equalsIgnoreCase("Welcome")) {
