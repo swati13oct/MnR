84f94fe4e275eaae1f0e7d95eb5bdafd2bbe0064
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-04-21 21:14:45 +0530

    F00000:US000000-For reportinggg

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddEob.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddEob.java
@@ -14,8 +14,8 @@ import cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.eob" }, 
 		features = { "feature/explanation-of-benefits" }, 
-		format = {
-		"pretty", "html:reports/test-report" }, tags ={"@F85974"})
+		format = { 
+		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@F85974"})
 public class RunMRAtddEob {
 
 }
