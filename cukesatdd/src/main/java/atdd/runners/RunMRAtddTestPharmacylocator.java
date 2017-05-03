9fc7a3c72d2d92a51301df28af0395477a7d602f
tpravee2 <praveenkumar_tanguturi@optum.com>
2017-05-03 12:54:45 +0530

    F70838:US459522-ATDD code changes for April release

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestPharmacylocator.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestPharmacylocator.java
@@ -8,15 +8,14 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 /**
- * @author pperugu 
+ * @author pperugu
  *
  */
 @RunWith(Cucumber.class)
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.pharmacylocator.member.ulayer" }, 
 		features = { "feature/pharmacy-locator/member/ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report", "json:target/cucumber.json" }, tags ={"@pharmacyprpnwidget,@pharmacysearchresult,@pharmacysearch"})
-
+		"pretty", "html:reports/test-report" }, tags ={"@pharmacyprpnwidget,@pharmacysearchresult,@pharmacysearch"})
 public class RunMRAtddTestPharmacylocator {
 
 }
