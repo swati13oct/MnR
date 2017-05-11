a7facd144a3097e6f55186fd0445128c83f3b40f
tpravee2 <praveenkumar_tanguturi@optum.com>
2017-05-11 14:50:26 +0530

    F85394:US562215-ATDD code changes for June release

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestFormsandResources.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestFormsandResources.java
@@ -15,7 +15,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.formsandresources.ulayer" }, 
 		features = { "feature/forms-and-resources/ulayer" }, 
 		format = {
-				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@drugtransistion,@privacypolicy"})
+				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@appeals,@udrugtrasition,@memberright,@seasonalflu,@termofuse,@medicaltherapy,@pharmacymail,@disenrollment"})
 public class RunMRAtddTestFormsandResources {
 
 }
