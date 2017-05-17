bb1e343bea93a4a1e15599a301057821ee44fa6d
tpravee2 <praveenkumar_tanguturi@optum.com>
2017-05-17 16:33:36 +0530

    F85394:US562215-ATDD code changes for June release

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestFormsandResources.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestFormsandResources.java
@@ -15,7 +15,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.formsandresources.bluelayer" }, 
 		features = { "feature/forms-and-resources/bluelayer" }, 
 		format = {
-				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@drugtransistion,@privacypolicy,@appeals,@udrugtrasition,@memberright,@seasonalflu,@termofuse,@medicaltherapy,@pharmacymail,@disenrollment,@medicationtherapy,@coverageappealspdp,@coverageappealsmapd,@preferedmailbenefit,@lowertierdruglearnmore"})
+				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@pharmacybenefit,@appoint,@privacypolicy,@noaccordion,@memberrightres,@mapdappealsandgrievances,@ssupappealsandgrievances,@medicaltherapyprog,@seasonflushot,@medicathreapymapd,@prescriptiondrug,@prescriptiondrug,@drugeob,@drugtransition,@disenrollment"})
 public class RunMRAtddTestFormsandResources {
 
 }
