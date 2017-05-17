f5ba481da25bcfb16f578055d414f28c144435a2
njain112 <nikita_jain@optum.com>
2017-05-17 17:49:25 +0530

    F78865:US588037-Value added services Nurse Helath Line

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBnC.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBnC.java
@@ -13,8 +13,8 @@ import cucumber.junit.Cucumber;
  */
 @RunWith(Cucumber.class)
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoveragejenkins.ulayer" }, 
-		features = { "feature/plan-benefits-and-coverage/ulayer" }, 
+		features = {"feature/plan-benefits-and-coverage/ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@availablehighOptionRidersjenkins,@activeRider3jenkins"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@availablehighOptionRidersjenkins"})
 public class RunMRAtddTestBnC {
 }
