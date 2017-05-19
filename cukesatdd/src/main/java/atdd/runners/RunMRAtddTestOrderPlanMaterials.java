870463307dfc606ce84c12c9f33e8cfe64788c93
mrani101 <rani_madadi@optum.com>
2017-05-19 19:53:34 +0530

    F85395:US5627579-ATDD code changes for Sprint 4 June release

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestOrderPlanMaterials.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestOrderPlanMaterials.java
@@ -14,7 +14,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.ordermaterials.bluelayer" }, 
 		features = { "feature/ordermaterials/bluelayer" }, 
 		format = {
-				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@radiobuttonvalidationmaandssup"})
+				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@radiobuttonvalidationmapdandmaindi"})
 
 public class RunMRAtddTestOrderPlanMaterials {
 	
