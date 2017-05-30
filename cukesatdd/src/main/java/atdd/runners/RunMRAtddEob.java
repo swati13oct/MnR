59ca5b2a012280f13735c418bd687123f07a36b4
Nidhi <nidhi_mishra@optum.com>
2017-05-30 16:00:15 +0530

    F85974:US593757-Committing latest code

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddEob.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddEob.java
@@ -12,10 +12,10 @@ import cucumber.junit.Cucumber;
  *
  */
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.eob" }, 
-		features = { "feature/explanation-of-benefits/Ulayer" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.dashboard.eob" }, 
+		features = { "feature/dashboard/eob" }, 
 		format = { 
-		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@eobStatments"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@F85974passed"})
 public class RunMRAtddEob {
 
 }
