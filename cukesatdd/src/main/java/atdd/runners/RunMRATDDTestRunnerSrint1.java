eb46a870c025d76edb0343a42cc3b17269e21e47
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-06-06 16:18:22 +0530

    F00000:US000000-Codes

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDTestRunnerSrint1.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDTestRunnerSrint1.java
@@ -8,7 +8,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.ulayer" }, 
 		features = { "feature/responsive/Ulayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@aprilReleaseSprint1"})
+		"pretty", "html:reports/test-report","json:target/cucumber-sprint1.json" }, tags ={"@aprilReleaseSprint1"})
 public class RunMRATDDTestRunnerSrint1 {
 
 }
