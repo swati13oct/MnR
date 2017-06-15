eaa2f1289c589dc794a1cad640d8f562875ecd1b
Nidhi <nidhi_mishra@optum.com>
2017-06-15 20:59:07 +0530

    F85974:US593757-Committing latest code

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
@@ -8,7 +8,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.bluelayer" }, 
 		features = { "feature/responsive/Blayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@enrollPlan"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@planhighlights"})
 public class RunMRATddTestResponsiveVPP {
 
 }
