b711476d0e432b55e9695e2d30833fbe72834671
Aakash Goyal <aakash_goyal@optum.com>
2017-06-19 19:58:56 +0530

    F100518:US657544-Miscellaneous

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
@@ -5,8 +5,8 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.bluelayer" }, 
-		features = { "feature/responsive/Blayer" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.ulayer" }, 
+		features = { "feature/responsive/Ulayer" }, 
 		format = {
 		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@US544283,@US544368,@US557073,@US519611,@US540565,@US519614,@US519618,@medicalbenefits,@PlanCosts,@OptionalServices,@Prescriptionbenefits"})
 public class RunMRATddTestResponsiveVPP {
