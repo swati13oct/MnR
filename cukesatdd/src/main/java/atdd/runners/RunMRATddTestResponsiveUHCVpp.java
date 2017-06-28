844f3ead284936a34f74ec6dfed50ab4ec52a251
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-06-28 19:55:41 +0530

    F00000:US000000-finxf

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveUHCVpp.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveUHCVpp.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.bluelayer" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.Blayer" }, 
 		features = { "feature/responsive/Blayer" }, 
 		format = {
 		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@sprint1"})
