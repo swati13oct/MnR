4ee829e5f0e49f3019f11b75325f421bc2ea4a52
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-06-30 00:05:44 +0530

    F00000:US000000-Fixing

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveUHCVpp.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveUHCVpp.java
@@ -9,6 +9,7 @@ import cucumber.junit.Cucumber;
 		features = { "feature/responsive/Blayer" }, 
 		format = {
 		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@suplementPlan"})
+
 public class RunMRATddTestResponsiveUHCVpp {
 
 }
