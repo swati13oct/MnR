f542b91492ae85584a2ae43bf626152877320de7
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-06-30 14:05:14 +0530

    F00000:US000000-EOb sprint

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddEob.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddEob.java
@@ -15,7 +15,8 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.dashboard.eob" }, 
 		features = { "feature/dashboard/eob" }, 
 		format = { 
-		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@F85974passed"})
+				
+		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@EOBSepsprint1"})
 public class RunMRAtddEob {
 
 }
