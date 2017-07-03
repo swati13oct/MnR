30447d959e57679dcd0637312f6a37c707abbcc4
Aakash Goyal <aakash_goyal@optum.com>
2017-07-03 17:42:40 +0530

    F51848:US622159-Miscellaneous updates

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.bluelayer" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.ulayer" }, 
 		features = { "feature/responsive/Blayer" }, 
 		format = {
 		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@US544283,@US544368,@US557073,@US519611,@US540565,@US519614,@US519618,@medicalbenefits,@PlanCosts,@OptionalServices,@Prescriptionbenefits"})
