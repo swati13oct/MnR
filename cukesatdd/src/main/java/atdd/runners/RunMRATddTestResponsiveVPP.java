996be734baa518bbf07a41082aad472dfbda9494
Nidhi <nidhi_mishra@optum.com>
2017-07-14 13:14:08 +0530

    F85974:US593757-Committing latest code

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveVPP.java
@@ -5,8 +5,8 @@ import org.junit.runner.RunWith;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.ulayer" }, 
-		features = { "feature/responsive/Ulayer" }, 
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.bluelayer" }, 
+		features = { "feature/responsive/Blayer" }, 
 		format = {
 		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@connectormodel"})
 
