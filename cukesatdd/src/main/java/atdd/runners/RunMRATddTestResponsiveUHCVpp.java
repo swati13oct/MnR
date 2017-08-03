7d815c93ee2e136f541b3dee45855de59622a817
anilesh priyadarshi <anilesh_priyadarshi@optum.com>
2017-08-03 13:50:08 +0530

    F00000:US000000-Runner class tags

--- cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveUHCVpp.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATddTestResponsiveUHCVpp.java
@@ -8,7 +8,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.responsive.bluelayer" }, 
 		features = { "feature/responsive/Blayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@US674468"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json" }, tags ={"@sprint2", "@planYearToggle", "@rightRail", "@planCount12", "@suplementPlan", "@addtocomparenotdisplayed", "@enrollnowforsnpnotdisplayed",  "@benefittable", "@benefittablema", "@learnmore", "@rightRail", "@US699059", "@connectormodel"})
 
 public class RunMRATddTestResponsiveUHCVpp {
 	
