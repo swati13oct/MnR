592a98f20c2e7c26e11915fdf07714e6fce7671f
sarora29 <somya_arora@optum.com>
2017-08-04 13:27:47 +0530

    F0000:US000000-Runner file

---
+++ cukesatdd/src/main/java/atdd/runners/dashboard/RunMRAtddTestRegistrationErrorsDashboard.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package atdd.runners.dashboard;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+/**
+ * @author akuma103
+ *
+ */
+@RunWith(Cucumber.class)
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.dashboard.registration" }, 
+		features = { "feature/registrationMemberRedesign" }, 
+		format = {
+		"pretty", "html:reports/test-report","json:target/RunJarvisCloaking-cucumber.json"}, tags ={"@pffsMemberErrorPage, @existingMemberErrorPage ,@futureEffectivePlanError,@inactiveTerminatedPlanError ,@memberInfoNotFoundError"})
+public class RunMRAtddTestRegistrationErrorsDashboard {
+
+}
