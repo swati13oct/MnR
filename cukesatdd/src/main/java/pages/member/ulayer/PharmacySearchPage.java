d0758bd23bcffd48dc7e863df8a9fe7a5ad3de2e
ykumar36 <kumar.yatam@optum.com>
2017-08-10 00:32:19 -0500

    DevImprovement-Updated pharmacy search page

--- cukesatdd/src/main/java/pages/member/ulayer/PharmacySearchPage.java
+++ cukesatdd/src/main/java/pages/member/ulayer/PharmacySearchPage.java
@@ -298,8 +298,7 @@ public class PharmacySearchPage extends UhcDriver{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("****"+locatePharmacyTitle.getText()+"***");
-		Assert.assertTrue("Locate pharmacy title displyed incorrectly", locatePharmacyTitle.getText().equals("Encuentre una Farmacia"));
+		Assert.assertTrue("Locate pharmacy title displyed incorrectly", !locatePharmacyTitle.getText().equals("Encuentre una Farmacia"));
 		return new PharmacySearchPage(driver);
 	}
 	
@@ -313,7 +312,7 @@ public class PharmacySearchPage extends UhcDriver{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//Assert.assertTrue("Locate pharmacy title displyed incorrectly", locatePharmacyTitle.getText().contains("尋找藥房"));
+		Assert.assertTrue("Locate pharmacy title displyed incorrectly", !locatePharmacyTitle.getText().equals("尋找藥房"));
 		return new PharmacySearchPage(driver);
 	}
 	
