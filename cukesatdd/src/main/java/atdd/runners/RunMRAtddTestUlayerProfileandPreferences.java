322bc78c8258fc792d9a1d31fe392b415be25e3c
Nikita Jain <nikita_jain@optum.com>
2017-09-20 19:58:01 +0530

    F118843:US745365-UHC FED member Paper Less Preference

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestUlayerProfileandPreferences.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestUlayerProfileandPreferences.java
@@ -12,7 +12,7 @@ import cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 @Cucumber.Options(glue = { "atdd.framework", "acceptancetests.ProfileandPreferencesredesign.ulayer" }, features = {
 		"feature/myprofile-preferences/ulayer" }, format = { "pretty", "html:reports/test-report",
-				"json:target/cucumber.json" }, tags = {"@CommunicationPreferences,@ValidatePlanNamemembernameIDAccountSectionAARP,@ValidateEmail,@PermanentAddress"})
+				"json:target/cucumber.json" }, tags = {"@EmailEdit2,@EmailEdit1,@CommunicationPreferences,@ValidatePlanNamemembernameIDAccountSectionAARP,@ValidateEmail,@PermanentAddress"})
 public class RunMRAtddTestUlayerProfileandPreferences
 {
 }
