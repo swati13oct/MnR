c2780f869da33dc748c889d248889bbb834f4832
Sneha Dwarakanath <sneha.dwarakanath@optum.com>
2017-10-02 19:36:55 -0400

    F101429:US669518-ATDD fixing code for Content stories for Forms and Resources BlueLayer

---
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestFormsandResourcesBlueLayer.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package atdd.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+/**
+ * @author pperugu
+ *
+ */
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.formsandresources.bluelayer" }, 
+		features = { "feature/forms-and-resources/bluelayer" }, 
+		format = {
+				"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@drugtransistion,@privacypolicy,@appeals,@udrugtrasition,@memberright,@seasonalflu,@termofuse,@medicaltherapy,@pharmacymail,@disenrollment,@medicationtherapy,@coverageappealspdp,@coverageappealsmapd,@preferedmailbenefit,@lowertierdruglearnmore"})
+public class RunMRAtddTestFormsandResourcesBlueLayer {
+
+}
+
