a897309c7a95394dbeb0f1f969cf282428b6442a
akapoo18 <alisha_kapoor@optum.com>
2017-10-10 16:30:07 +0530

    F104844:US746037- aarp blue layer Add Temp Address

---
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBlayerNeedHelp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package atdd.runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+/**
+ * @author pjaising
+ *
+ */
+@RunWith(Cucumber.class)
+@Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoveragejenkins.bluelayer"}, 
+		features = {"feature/plan-benefits-and-coverage/bluelayer" }, 
+		format = {
+		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@needHelpums"})
+public class RunMRAtddTestBlayerNeedHelp {
+}
