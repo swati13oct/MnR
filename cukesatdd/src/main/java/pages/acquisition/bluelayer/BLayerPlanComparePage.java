7bf9c958f76e31925eb1a08520104c7cfae2add6
Kartheek <kartheek_gosi@optum.com>
2017-10-16 16:42:25 +0530

    F00000:US123456-xpathchanges

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/BLayerPlanComparePage.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/BLayerPlanComparePage.java
@@ -33,11 +33,11 @@ public class BLayerPlanComparePage extends UhcDriver {
 //	@FindBy(xpath = "//*[@id='plan-list-1']/div/div[2]/div/div[2]/div[3]/div/div/span[1]/label")	                 
 //	private WebElement Plan1HMO1;
 	
-//	@FindBy(xpath = "//*[@id='plan-list-1']/div/div[3]/div/div[2]/div[3]/div/div/span[3]/a")
-	@FindBy(xpath = "(.//a[contains(text(),'Compare plans')])[2]")
+	@FindBy(xpath = "//*[@id='plan-list-1']/div/div[2]/div/div[2]/div[3]/div/div/span[3]/a")
+//	@FindBy(xpath = "(.//a[contains(text(),'Compare plans')])[2]")
 	private WebElement ComparePlansLink;	
 	
-//	@FindBy(xpath = "//*[@id='plan-list-1']/div/div[2]/div/div[2]/div[3]/div/div/span[3]/a")	                  
+//	@FindBy(xpath = "//*[@id='plan-list-1']/div/div[3]/div/div[2]/div[3]/div/div/span[3]/a")	                  
 //	private WebElement ComparePlansLink1;	
 	
 	@FindBy(xpath = "//*[@id='plan-list-1']/div/div[3]/div/div[2]/div[3]/div/div/span[3]/a")	                 
