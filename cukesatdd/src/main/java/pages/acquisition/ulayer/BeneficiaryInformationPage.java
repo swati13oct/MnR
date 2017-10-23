08673dadc27d4f0ece50b320d4448b649ca394ef
ashah120 <aayush.shah@optum.com>
2017-10-23 15:45:10 -0400

    DevImprovement-VPP blayer flow and OLE aarp flow updated

--- cukesatdd/src/main/java/pages/acquisition/ulayer/BeneficiaryInformationPage.java
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/BeneficiaryInformationPage.java
@@ -303,8 +303,8 @@ public class BeneficiaryInformationPage extends UhcDriver{
 	
 	public boolean validateBeneficiaryPage(){
 		boolean flag = false;
-		if(validate(dateOfBirthField)&&validate(maleRadioButton)&&validate(femaleRadioButton)&&validate(address1Field)&&validate(address2Field)
-				&&validate(cityField)&& validate(stateField)&& validate(zipcodeField)&&validate(mailingAddressYesButton)&&validate(mailingAddressNoButton)
+		if(validate(dateOfBirthField)&&validate(address1Field)&&validate(address2Field)
+				&&validate(cityField)&& validate(stateField)&& validate(zipcodeField)
 				&&validate(emailAddressField)&& validate(enrollmentNext)&&validate(primaryPhoneNumberField)&&validate(alternatePhoneNumberlink)&&validate(beneficiaryCancelBtn))
 			flag= true;
 		return flag;
