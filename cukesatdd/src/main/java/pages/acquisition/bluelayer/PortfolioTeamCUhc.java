25393ae7c572518796cbbd965406b260a4d00b5b
Kartheek <kartheek_gosi@optum.com>
2017-10-23 15:16:06 +0530

    F00000:US123456-xpath

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/PortfolioTeamCUhc.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/PortfolioTeamCUhc.java
@@ -13,11 +13,10 @@ import acceptancetests.atdd.data.MRConstants;
 import atdd.framework.UhcDriver;
 
 public class PortfolioTeamCUhc extends UhcDriver {
-	
+
 	@FindBy(id = "cta-zipcode")
 	private WebElement zipCodeField;
-	
-	
+
 	private static String PAGE_URL = MRConstants.TeamC_UHC_VPP_URL;
 
 	public PortfolioTeamCUhc(WebDriver driver) {
@@ -28,40 +27,38 @@ public class PortfolioTeamCUhc extends UhcDriver {
 
 	@Override
 	public void openAndValidate() {
-		start(PAGE_URL);		
+		start(PAGE_URL);
 	}
 
-	
-	public ResponsivePlanSummaryUhc searchPlans(String zipcode, String County) {
-	    
-	    sendkeys(zipCodeField, zipcode);
-	    zipCodeField.sendKeys(Keys.ENTER);
-	    //remove thread once page is stable
-	    try {
+	public ResponsivePlanSummaryUhc searchPlans(String zipcode, String County) throws InterruptedException {
+		Thread.sleep(5000);
+		sendkeys(zipCodeField, zipcode);
+		zipCodeField.sendKeys(Keys.ENTER);
+		// remove thread once page is stable
+		try {
 			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-	    /*List<WebElement> countyActuals = driver.findElements(By.xpath("//a[@class='ng-binding ng-pristine ng-valid']"));
-	    System.out.println(countyActuals.size());
-	    
-	    for(int i=0; i<=countyActuals.size()-1;i++){
-	    	System.out.println(CountyName);
-	    	if(countyActuals.get(i).getText().equals(CountyName)){
-	    		System.out.println(CountyName);
-	    		System.out.println(countyActuals.get(i).getText());
-	    		countyActuals.get(i).click();
-	    		break;
-	    	}
-	    }*/
+
+		/*
+		 * List<WebElement> countyActuals = driver.findElements(By.xpath(
+		 * "//a[@class='ng-binding ng-pristine ng-valid']"));
+		 * System.out.println(countyActuals.size());
+		 * 
+		 * for(int i=0; i<=countyActuals.size()-1;i++){
+		 * System.out.println(CountyName);
+		 * if(countyActuals.get(i).getText().equals(CountyName)){
+		 * System.out.println(CountyName);
+		 * System.out.println(countyActuals.get(i).getText());
+		 * countyActuals.get(i).click(); break; } }
+		 */
 		if (driver.getTitle().contains("Our Medicare Plan Types")) {
 			return new ResponsivePlanSummaryUhc(driver);
-		} 
+		}
 		return null;
-	
-}
-	
-	
+
+	}
+
 }
