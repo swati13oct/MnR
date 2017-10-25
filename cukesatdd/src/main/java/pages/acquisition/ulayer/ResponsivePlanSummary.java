ad37cc5a1f8a3979eaeb39b473641d0755fa1871
Kartheek <kartheek_gosi@optum.com>
2017-10-25 13:08:07 +0530

    F00000:US000000-showMaPlans xpath

--- cukesatdd/src/main/java/pages/acquisition/ulayer/ResponsivePlanSummary.java
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/ResponsivePlanSummary.java
@@ -174,8 +174,8 @@ public class ResponsivePlanSummary extends UhcDriver{
 		    @FindBy (xpath=".//*[@id='fixTable']/tbody/tr[3]/td[4]")
 		    private WebElement outofpocket2;
 		    
-		   // @FindBy(xpath="(.//span[contains(text(),'View Plans')][@class='trigger-closed'])[1]")
-		    @FindBy(xpath=".//*[@id='site-wrapper']/div[4]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div/span[3]")
+		    @FindBy(xpath="(.//span[contains(text(),'View Plans')][@class='trigger-closed'])[1]")
+		//  @FindBy(xpath=".//*[@id='site-wrapper']/div[4]/div/div[1]/div/div/div/div/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div/span[3]")
 		    private WebElement showMaPlans;
 			
 			@FindBy(xpath="//*[contains(text(),'Medicare Prescription')]/following-sibling::span[2]")
