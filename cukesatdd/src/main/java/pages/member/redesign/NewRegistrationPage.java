c704bff9c29d604649635d334ac574b4a2970f11
bnaveen4 <naveen_bk@optum.com>
2017-10-26 17:36:59 +0530

    F00000:US00000-updated the go green fix

--- cukesatdd/src/main/java/pages/member/redesign/NewRegistrationPage.java
+++ cukesatdd/src/main/java/pages/member/redesign/NewRegistrationPage.java
@@ -77,7 +77,7 @@ public class NewRegistrationPage extends UhcDriver {
 			JavascriptExecutor js = (JavascriptExecutor) driver;
 			sendkeys(memberIdField, memberId);
 			driver.findElement(By.xpath("//span[@id='select2-date-mm-container']/following::span[1]")).click();
-			if(strMonth!="01")
+			if(!strMonth.equals("01"))
 				driver.findElement(By.xpath("//ul[@id='select2-date-mm-results']//li[(@class='select2-results__option') and text()='"+strMonth+"']")).click();
 			else
 				driver.findElement(By.xpath("//ul[@id='select2-date-mm-results']//li[(@class='select2-results__option select2-results__option--highlighted')]")).click();
