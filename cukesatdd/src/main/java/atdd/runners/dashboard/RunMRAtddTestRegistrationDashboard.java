df79008f52cf078017e724b211de2acee53a71d4
sarora29 <somya_arora@optum.com>
2017-11-10 19:09:03 +0530

    F0000:US000000-ATDD Fixes

--- cukesatdd/src/main/java/atdd/runners/dashboard/RunMRAtddTestRegistrationDashboard.java
+++ cukesatdd/src/main/java/atdd/runners/dashboard/RunMRAtddTestRegistrationDashboard.java
@@ -12,7 +12,7 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.dashboard.registration" }, 
 		features = { "feature/registrationMemberRedesign" }, 
 		format = {"pretty", "html:reports/test-report","json:target/RunMRAtddTestRegistrationDashboard-cucumber.json"  },
-		tags ={"@stepOneMemberErrorPage,registrationRedesign"})
+		tags ={"@registrationRedesign"})
 public class RunMRAtddTestRegistrationDashboard {
 
 }
