b3a800b4873aa3d7d9e01f193e253216f67d678d
bnaveen4 <naveen_bk@optum.com>
2017-11-14 15:55:00 +0530

    F00000:US00000-Updated the stage url

--- cukesatdd/src/main/java/acceptancetests/atdd/data/MRConstants.java
+++ cukesatdd/src/main/java/acceptancetests/atdd/data/MRConstants.java
@@ -17,7 +17,7 @@ public class MRConstants {
 	//public static final String UHCM_URL = "https://member."+MRScenario.environment+"-uhcmedicaresolutions.uhc.com/guest/home.html";
 
 	public static final String AARPM_URL = "https://member."+MRScenario.environment+"-aarpmedicareplans.uhc.com/guest/login.html";
-	public static final String NEW_REDESIGN_URL = "https://"+MRScenario.environment+"medicare.uhc.com/login/overview.html?testharness=true";
+	public static final String NEW_REDESIGN_URL = "https://"+MRScenario.environment+"-medicare.uhc.com/login/overview.html?testharness=true";
 	public static final String MEMBER_AUTH_REDESIGN_LOGIN_URL = "https://"+MRScenario.environment+"-medicare.uhc.com/content/dashboard/guest/memberauth.html#/memberAuthLogin";
 	public static final String NEW_REDESIGN_REGISTRATION_URL = "https://"+MRScenario.environment+"-medicare.uhc.com/content/medicare/member-registration.html#/memberRegistration";
 	
