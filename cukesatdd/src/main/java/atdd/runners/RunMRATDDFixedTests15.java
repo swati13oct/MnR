64af0e9a784b0885be992d1238c2bdbd4c22b6b9
ykumar36 <kumar.yatam@optum.com>
2017-11-14 09:33:11 -0600

    DevImprovement-Updated ssllabs tags in runner

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDFixedTests15.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDFixedTests15.java
@@ -16,7 +16,7 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=2,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDFixedTests15.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDFixedTests15")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.fixedtestcases" }, features = { "src/main/resources/feature/fixedtestcases" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDFixedTests15.json" }, tags = { "@globalfooter,@attendCommunity,@AcqSEO,@druglookup,@druglookupUms,@acq_drug_cost_estimator,@acq_drug_cost_estimator_blayer" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDFixedTests15.json" }, tags = { "@globalfooter,@attendCommunity,@AcqSEO,@druglookup,@druglookupUms,@acq_drug_cost_estimator,@acq_drug_cost_estimator_blayer,@ssllabs" })
 public class RunMRATDDFixedTests15 {
 
 }
