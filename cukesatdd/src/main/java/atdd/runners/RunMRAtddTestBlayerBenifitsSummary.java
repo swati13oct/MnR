20351566cff2eed49257e5846c6dd1a577eae599
Nikita Jain <nikita_jain@optum.com>
2017-12-20 16:49:28 +0530

    F104838:US664161-AARP FED MAPD MEMBER Plan Documents

--- cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBlayerBenifitsSummary.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRAtddTestBlayerBenifitsSummary.java
@@ -15,6 +15,6 @@ import cucumber.junit.Cucumber;
 @Cucumber.Options(glue = { "atdd.framework","acceptancetests.benefitsandcoveragejenkins.bluelayer"}, 
 		features = {"feature/plan-benefits-and-coverage/bluelayer" }, 
 		format = {
-		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={"@BncHeadersGroup,@BncHeadersIndividual"})
+		"pretty", "html:reports/test-report","json:target/cucumber.json"}, tags ={" @CodeMonkeysvalidatePdfsectiongroupenglish,@CodeMonkeysvalidatePdfsectiongroupspanishchinese,@CodeMonkeysvalidatePdfsectionindividual"})
 public class RunMRAtddTestBlayerBenifitsSummary {
 }
