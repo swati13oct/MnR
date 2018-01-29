e9ed287ed8affeabc7fd31fb7842cd6e176f572e
agarg119 <ankit_garg1@optum.com>
2018-01-29 13:33:07 +0530

    DevImprovement-Updated Stage.config.properties Webdrver key

---
+++ cukesatdd/src/main/java/atdd/runners/SanityRunRedesignConsolidatedRunner.java
@@ -0,0 +1,16 @@
+package atdd.runners;
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberrdesign" }, features = { "src/main/resources/feature/memberrdesign" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber.json" }, tags = { "@smokeTest" },monochrome = true)
+
+public class SanityRunRedesignConsolidatedRunner {
+
+}
