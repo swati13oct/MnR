ca76d2040360f68fcfbb49c14a5b7646853900bc
Nikita Jain <nikita_jain@optum.com>
2018-02-26 17:58:58 +0530

    F00001:US00004-MRScenario Change

--- cukesatdd/src/main/java/atdd/framework/MRScenario.java
+++ cukesatdd/src/main/java/atdd/framework/MRScenario.java
@@ -718,7 +718,7 @@ public class MRScenario {
                }
 */           
                public WebDriver getWebDriver() {
-               DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+               DesiredCapabilities capabilities = DesiredCapabilities.chrome();
                                              
                capabilities.setCapability("platform", "Windows 7");
                capabilities.setCapability("version", "45.0");
