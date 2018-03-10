881e912eaaa0425042d3e171a6e7edd67801f0c7
agarg119 <ankit_garg1@optum.com>
2018-03-10 23:27:41 +0530

    DevImprovement-Updated runner and config file

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDMemberRedesignSanityVBF.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDMemberRedesignSanityVBF.java
@@ -9,7 +9,7 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=2, screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberrdesignVBF" }, features = { "src/main/resources/feature/member-redesign-vbf" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunnerRedesignSanityTest.json" }, tags = { "@smokeTest" },monochrome = true)
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDMemberRedesignSanityVBF.json" }, tags = { "@smokeTest" },monochrome = true)
 
 public class RunMRATDDMemberRedesignSanityVBF {
 
