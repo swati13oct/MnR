4e918a9005b66971f835d80d3a2586d1df7dc00a
Sneha Dwarakanath <sneha.dwarakanath@optum.com>
2018-03-20 19:32:50 -0400

    F142720:US1006795-changes to code and team runner and April runner for April Sprint 4 US

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAprilRelease.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAprilRelease.java
@@ -8,12 +8,12 @@ import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
 
 /**
- * this file is to run test cases for RunMRATDDAcquisitionDCE
+ * this file is to run test cases for April Release User Stories in Acquisition
  */
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=2,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAprilRelease.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAprilRelease")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests" }, features = { "src/main/resources/feature" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAprilRelease.json" }, tags = { "@aprilRelease2018" })
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisitionvbf" }, features = { "src/main/resources/feature/acquisitionvbf" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAprilRelease.json" }, tags = { "@aprilRelease2018"})
 public class RunMRATDDAprilRelease { 
  
 }
