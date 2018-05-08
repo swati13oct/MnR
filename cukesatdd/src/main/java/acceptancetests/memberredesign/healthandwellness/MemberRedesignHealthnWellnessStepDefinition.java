3c55fbb7f4e3bb151c4548ad0cea0396ff2d0d82
chaitanya <chaitanya_koppuravuri@optum.com>
2018-05-08 17:44:30 +0530

    F00000:US123456-Fixed

--- cukesatdd/src/main/java/acceptancetests/memberredesign/healthandwellness/MemberRedesignHealthnWellnessStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/memberredesign/healthandwellness/MemberRedesignHealthnWellnessStepDefinition.java
@@ -6,11 +6,7 @@ import acceptancetests.data.CommonConstants;
 import acceptancetests.data.PageConstantsMnR;
 import atdd.framework.MRScenario;
 import cucumber.api.java.en.When;
-//import pages.dashboard.member.drugcostestimator.blayer.DrugCostEstimatorPage;
-//import pages.memberredesign.bluelayer.AccountHomePage;
-//import pages.memberredesign.bluelayer.HealthAndWellness;
 import pages.regression.healthandwellness.HealthAndWellnessPage;
-//import pages.memberredesign.bluelayer.LoginPage;
 
 /**
  * Functionality : Covers step definition methods related to member redesign Health and Wellness page .
@@ -25,78 +21,6 @@ public class MemberRedesignHealthnWellnessStepDefinition {
 	}
 
 	/**
-	 * @toDo : Finds authenticated user to login with Health n Wellness rewards
-	 * @param memberAttributes
-	 */
-	/*@Given("^I am a authenticated member on the member redesign site HW$")
-	public void I_am_a_authenticated_member_on_the_member_redesign_site(DataTable memberAttributes) {
-		WebDriver wd = getLoginScenario().getWebDriver();
-		getLoginScenario().saveBean(CommonConstants.WEBDRIVER, wd);
-		List<DataTableRow> memberAttributesRow = memberAttributes
-				.getGherkinRows();
-		Map<String, String> memberAttributesMap = new LinkedHashMap<String, String>();
-		for (int i = 0; i < memberAttributesRow.size(); i++) {
-
-			memberAttributesMap.put(memberAttributesRow.get(i).getCells()
-					.get(0), memberAttributesRow.get(i).getCells().get(1));
-		}
-
-		String category = memberAttributesMap.get("Member Type");
-
-		Set<String> memberAttributesKeySet = memberAttributesMap.keySet();
-		List<String> desiredAttributes = new ArrayList<String>();
-		for (Iterator<String> iterator = memberAttributesKeySet.iterator(); iterator
-				.hasNext();) {
-			{
-				String key = iterator.next();
-				desiredAttributes.add(memberAttributesMap.get(key));
-			}
-
-		}
-		System.out.println("desiredAttributes.." + desiredAttributes);
-
-		Map<String, String> loginCreds = loginScenario
-				.getUMSMemberWithDesiredAttributes(desiredAttributes);
-
-		String userName = null;
-		String pwd = null;
-		if (loginCreds == null) {
-			// no match found
-			System.out.println("Member Type data could not be setup !!!");
-			Assert.fail("unable to find a " + desiredAttributes + " member");
-		} else {
-			userName = loginCreds.get("user");
-			pwd = loginCreds.get("pwd");
-			System.out.println("User is..." + userName);
-			System.out.println("Password is..." + pwd);
-			getLoginScenario()
-			.saveBean(LoginCommonConstants.USERNAME, userName);
-			getLoginScenario().saveBean(LoginCommonConstants.PASSWORD, pwd);
-			getLoginScenario().saveBean(LoginCommonConstants.CATOGERY, category);
-		}
-		DrugCostEstimatorPage dce = new DrugCostEstimatorPage(wd);
-		getLoginScenario().saveBean(PageConstants.DRUG_COST_ESTIMATOR_PAGE, dce);
-
-	}
-*/
-	/**
-	 * @toDo : User gets logged in to member site
-	 */
-	/*@When("^the above plantype user logs in UMS Site Desktop HW$")
-	public void plantype_user_logs_in() {
-		String userName = (String) getLoginScenario().getBean(LoginCommonConstants.USERNAME);
-		String pwd = (String) getLoginScenario().getBean(LoginCommonConstants.PASSWORD);
-		String category = (String) getLoginScenario().getBean(LoginCommonConstants.CATOGERY);
-		WebDriver wd = (WebDriver) getLoginScenario().getBean(CommonConstants.WEBDRIVER);
-		//LoginPage loginPage = new LoginPage(wd);
-		//loginPage.loginToStageTestHarness();
-		getLoginScenario().saveBean(PageConstantsMnR.LOGIN_PAGE, loginPage);
-		AccountHomePage accountHomePage = (AccountHomePage) loginPage.thloginWith(userName, pwd,category);
-		getLoginScenario().saveBean(PageConstantsMnR.ACCOUNT_HOME_PAGE, accountHomePage);
-
-	}
-*/
-	/**
 	 * @toDo : View Health n Wellness Global Navigation
 	 * @throws InterruptedException
 	 */
@@ -106,7 +30,7 @@ public class MemberRedesignHealthnWellnessStepDefinition {
 		WebDriver wd = (WebDriver) getLoginScenario().getBean(CommonConstants.WEBDRIVER);
 		HealthAndWellnessPage healthnWellnessPage = new HealthAndWellnessPage(wd);
 		healthnWellnessPage.validateHeaderOnDashborad();
-		
+
 		getLoginScenario().saveBean(PageConstantsMnR.MEM_REDESIGN_HEALTH_AND_WELLNESS_PAGE, healthnWellnessPage);
 	}
 
