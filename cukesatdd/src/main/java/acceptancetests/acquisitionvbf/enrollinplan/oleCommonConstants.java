ed36cc13f4849ef099922148a72d051cc28d158f
Aayush Shah <aayush.shah@optum.com>
2018-05-22 14:50:14 -0400

    DevImprovement-Adding ole common constants file to vbf folder

---
+++ cukesatdd/src/main/java/acceptancetests/acquisitionvbf/enrollinplan/oleCommonConstants.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package acceptancetests.acquisitionvbf.enrollinplan;
+
+/**
+ * @author sdwaraka
+ *
+ */
+public class oleCommonConstants {
+
+	//plan details
+	public static final String COMMON_DATA = "common";
+	public static final String OLE_PLAN_NAME = "planName";
+	public static final String OLE_PLAN_TYPE = "planType";
+	public static final String OLE_ZIPCODE = "zipcode";
+	public static final String OLE_COUNTY = "county";
+	public static final String OLE_PLAN_YEAR = "planyear";
+	public static final String OLE_PLAN_PREMIUM = "premium";
+	public static final String OLE_TFN = "tfn";
+	public static final String ACQ_SITE_NAME = "acq_site";
+	
+	//member details
+	public static final String FIRST_NAME = "firstname";
+	public static final String LAST_NAME = "lastname";
+	public static final String CARD_TYPE = "cardtype";
+	public static final String MEDICARE_NUMBER	 = "medicarenumber";
+	public static final String SSN_FLAG = "ssnflag";
+	public static final String PARTA_EFFECTIVE = "partaeffective";
+	public static final String PARTB_EFFECTIVE = "partbeffective";
+	public static final String SSN_NUMBER = "ssnnumber";
+
+}
