3780bcf9ccce915239db7cc2684681223c664de6
Aayush Shah <aayush.shah@optum.com>
2018-05-22 14:56:44 -0400

    DevImprovement-Runner file changed location of feature file

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionNewOLEVBF.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionNewOLEVBF.java
@@ -13,7 +13,7 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=2,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionNewOLEVBF.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionNewOLEVBF")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisitionvbf"},features={"src/main/resources/feature/acquisitionvbf.ole"},plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionNewOLEVBF.json"}, tags = {"@OLE_Ulayer,@OLE_UHC"})
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisitionvbf"},features={"src/main/resources/feature/acquisitionvbf"},plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionNewOLEVBF.json"}, tags = {"@OLE_Ulayer,@OLE_UHC"})
 public class RunMRATDDAcquisitionNewOLEVBF {
 
 }
