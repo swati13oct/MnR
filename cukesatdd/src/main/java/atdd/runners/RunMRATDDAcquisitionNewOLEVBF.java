167011746acdf68e244bd7c9648c672ad040486a
Aayush Shah <aayush.shah@optum.com>
2018-05-22 23:01:25 -0400

    DevImprovement-OLE runner file changed

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionNewOLEVBF.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionNewOLEVBF.java
@@ -11,9 +11,11 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionNewOLE
  */
 
+
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=2,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionNewOLEVBF.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionNewOLEVBF")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisitionvbf"},features={"src/main/resources/feature/acquisitionvbf"},plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionNewOLEVBF.json"}, tags = {"@OLE_Ulayer,@OLE_UHC"})
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisitionvbf" }, features = { "src/main/resources/feature/acquisitionvbf" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionNewOLEVBF.json" }, tags = { "@OLE_Ulayer,@OLE_UHC" })
 public class RunMRATDDAcquisitionNewOLEVBF {
 
 }
