1535413bdb9d2f5d0594830cc19f091f59058a3e
Garg, Ankit <ankit_garg1@optum.com>
2018-06-15 22:45:12 +0530

    DevImprovement-Updated test data for Team-ci

--- cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
+++ cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
@@ -851,7 +851,7 @@ public class CommonConstants {
     public static final String JENKINS_BROWSER_AGENT_STRING = "JENKINS_BROWSER_AGENT_STRING";
 	public static final String ACCOUNT_USER_NAME = null;
 
-	public static final String IS_TESTHARNESS = "istestharness";
+	public static final String IS_TESTHARNESS = "IS_TESTHARNESS";
 
 	public static final String COMMONSTEPDEFINITIONMEMVBF = "commonStepDefinition";
 	public static final String IS_HSID_COMPATIBLE	 = "isHSIDCompatible";
