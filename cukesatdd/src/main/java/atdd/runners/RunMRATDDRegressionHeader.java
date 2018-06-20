1e4f528aac29f678d0fee6bbcaf83c3a3cc45017
rubina <rubina.paul@optum.com>
2018-06-20 14:23:37 -0400

    DevImprovement-RegressionRunners functionality based

---
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDRegressionHeader.java
@@ -0,0 +1,25 @@
+package atdd.runners;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionHeader.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionHeader")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
+features = { "src/main/resources/feature/memberredesign" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionHeader.json" }, tags = { "@regression_06_06_18" })
+public class RunMRATDDRegressionHeader {
+
+}
---
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDRegressionHealthNWellness.java
@@ -0,0 +1,25 @@
+package atdd.runners;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionHealthNWellness.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionHealthNWellness")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
+features = { "src/main/resources/feature/memberredesign" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionHealthNWellness.json" }, tags = { "@regression_06_06_18" })
+public class RunMRATDDRegressionHealthNWellness {
+
+}
