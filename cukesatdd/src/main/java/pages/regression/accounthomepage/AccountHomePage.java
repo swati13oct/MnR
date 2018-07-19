878271196f3ed99181c044b01231d2a4117694c9
akapoo18 <alisha_kapoor@optum.com>
2018-07-19 14:05:48 +0530

    F00000:US00000-Benefits and coverage page

--- cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
@@ -1948,6 +1948,59 @@ public BenefitsAndCoveragePage navigateDirectToBnCPag() {
 		
 	}
 
+public BenefitsAndCoveragePage navigateToBandCPag() {
+	
+	if (MRScenario.environmentMedicare.equalsIgnoreCase("stage")) {
+		System.out.println("user is on Stage login page");
+		// CommonUtility.waitForPageLoad(driver, claimsDashboardLink, 90);
+		if (driver.getCurrentUrl().contains("/dashboard"))
+			;
+		{
+			System.out.println("User is on dashboard page and URL is ==>" + driver.getCurrentUrl());
+			
+			driver.navigate().to(PAGE_URL + "medicare/member/benefits-coverage.html");
+			try {
+				Thread.sleep(20000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(driver.getCurrentUrl());
+			//CommonUtility.waitForPageLoad(driver, heading, 30);
+			if (driver.getTitle().contains("Plan Benefits Summary")) {
+				System.out.println(driver.getTitle());
+				return new BenefitsAndCoveragePage(driver);
+			}
+
+		}
+	}
+
+	else if (MRScenario.environmentMedicare.equals("team-h") || MRScenario.environmentMedicare.equals("test-a")
+			|| MRScenario.environmentMedicare.equals("team-e")) {
+
+		driver.navigate().to(PAGE_URL + "medicare/member/benefits-coverage.html");
+		System.out.println(driver.getCurrentUrl());
+	} else {
+		driver.navigate().to(
+				"https://team-ci1-medicare.ose-elr-core.optum.com/content/medicare/member/benefits/overview.html");
+
+		System.out.println(driver.getCurrentUrl());
+	}
+
+	/*
+	 * if (validate(iPerceptionPopUp)) { iPerceptionPopUp.click();
+	 * System.out.println("iPerception Pop Up displayed"); }
+	 */
+
+	//CommonUtility.waitForPageLoad(driver, heading, 50);
+	if (driver.getTitle().equalsIgnoreCase("Plan Benefits Summary")) {
+		return new BenefitsAndCoveragePage(driver);
+	}
+
+	return null;
+	
+}
+
 }
 
  			
