d68671fa41d0b2bc57d0e2ea3b62f4305475952b
Aayush Shah <aayush.shah@optum.com>
2018-07-24 22:28:57 -0400

    DevImprovement-Applitools runner file updated to run in parallel

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDApplitoolsTest.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDApplitoolsTest.java
@@ -2,7 +2,7 @@ package atdd.runners;
 
 import org.junit.runner.RunWith;
 
-import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedParallelCucumber;
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 
 import cucumber.api.CucumberOptions;
@@ -13,13 +13,13 @@ import cucumber.api.CucumberOptions;
  *
  */
 
-@RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+@RunWith(ExtendedParallelCucumber.class)
+@ExtendedCucumberOptions(threadsCount=15,screenShotSize="", screenShotLocation="/screenshots/",
 jsonReport = "target/RunMRATDDApplitoolsTest.json",detailedReport = true, detailedAggregatedReport = true,
 overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDApplitoolsTest")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.vbfacquisition" }, 
 features = { "src/main/resources/feature/vbfacquisition" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDApplitoolsTest.json" }, tags = { "@applitoolsAARP_VPP,@applitoolsAARP_DCE,@applitoolsAARP_OLE" })
+		 "html:reports/test-report", "json:target/cucumber-RunMRATDDApplitoolsTest.json" }, tags = { "@applitoolsAARP_VPP,@applitoolsAARP_DCE,@applitoolsAARP_OLE" })
 public class RunMRATDDApplitoolsTest {
 
 }
