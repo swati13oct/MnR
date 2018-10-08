8dbe2b4fd110f038d4ca89fe87127d6caf79e143
rubina <rubina.paul@optum.com>
2018-10-08 12:29:59 -0400

    DevImprovement-commit for provider search AARP

--- cukesatdd/src/main/java/pages/acquisition/ulayer/VPPPlanSummaryPage.java
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/VPPPlanSummaryPage.java
@@ -188,7 +188,8 @@ public class VPPPlanSummaryPage extends UhcDriver {
 	@FindBy(id="label_unsaved_selectedLocation0")
 	private WebElement firstLocation;
 
-	@FindBy(xpath="//button[@class='action-btn']")
+	//@FindBy(xpath="//button[@class='action-btn']")
+	@FindBy(xpath="//*[@id='skip-to-main-content']/div/div[1]/div[1]/location-result/div/div/div/div[2]/div[2]/div[1]/div/div/div[3]/toggle-saved-provider/button/span")
 	private WebElement secondSaveBtn;
 	
 	@FindBy(xpath="//*[contains(text(),'View Saved')]")
