0c0e87540b2cf5be201c9b27618cb299427cf52f
rubina <rubina.paul@optum.com>
2018-11-07 14:33:26 -0500

    DevImprovment-updating the users

--- cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
@@ -217,8 +217,8 @@ public class AccountHomePage extends UhcDriver {
 	@FindBy(css = ".claimDetTableMainSection")
 	private WebElement claimDetTableMainSection;
 
-	//@FindBy(xpath = "//*[@id='dashboard']//span[text()='View Your Claims']")
-	@FindBy(xpath = "//*[@id='claims_1']")
+	@FindBy(xpath = "//*[@id='dashboard']//span[text()='View Your Claims']")
+	//@FindBy(xpath = "//*[@id='claims_1']")
 	private WebElement claimsDashboardLink;
 
 	@FindBy(xpath = "//span[contains (text(), 'Look up Drugs')]")
