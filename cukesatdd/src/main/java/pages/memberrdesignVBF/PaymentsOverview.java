c81ef2af6c7ddb75f874c8ec50c3c1e4064c7e2c
Aakash Goyal <aakash_goyal@optum.com>
2018-12-19 23:38:01 +0530

    F00000:US000000-Updated Payment VBF

--- cukesatdd/src/main/java/pages/memberrdesignVBF/PaymentsOverview.java
+++ cukesatdd/src/main/java/pages/memberrdesignVBF/PaymentsOverview.java
@@ -13,7 +13,7 @@ import atdd.framework.UhcDriver;
 
 public class PaymentsOverview extends UhcDriver {
 	// updated element locator
-	@FindBy(xpath = "//div[@class='margin-small']//a[contains(@ng-click,'gotoOneTimePayment()') and not(contains(@class,'ng-hide'))]")
+	@FindBy(xpath = "//div[@class='margin-small']//a[contains(@ng-click,'gotoMakeOneTimePayment()') and not(contains(@class,'ng-hide'))]")
 	private WebElement OneTimePaymentButton;
 
 	@FindBy(id = "paymentTable")
