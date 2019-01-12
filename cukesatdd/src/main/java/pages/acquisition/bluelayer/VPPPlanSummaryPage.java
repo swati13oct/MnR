389c093c7cb16f67e2605e2ec95c591ec5f9a201
Sneha Dwarakanath <sneha.dwarakanath@optum.com>
2019-01-12 13:49:41 -0500

    F00000:US000000-Removed wait for Element as session is timing out in Jenkins

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/VPPPlanSummaryPage.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/VPPPlanSummaryPage.java
@@ -486,7 +486,7 @@ public class VPPPlanSummaryPage extends UhcDriver {
 
 		}else if (planName.contains("HMO") || planName.contains("Regional PPO")) {
 			WebElement MAmoreDetailsLink = driver.findElement(By.xpath("//*[contains(text(), '"+planName+"')]/ancestor::div[@class='module-plan-overview module swiper-slide ng-scope']//a[contains(text(),'View plan and drug coverage details')]"));
-			CommonUtility.waitForPageLoad(driver, MAmoreDetailsLink, 30);	
+			//CommonUtility.waitForPageLoad(driver, MAmoreDetailsLink, 30);	
 					validate(MAmoreDetailsLink);
 					
 					MAmoreDetailsLink.click();
