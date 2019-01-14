1d36e1bf56e4a962fe87a233dd80309e3c15be29
Pooja <minhas.pooja@optum.com>
2019-01-14 14:45:28 -0500

    US 12345 F123456 Claims id changed

--- cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
@@ -228,7 +228,8 @@ public class AccountHomePage extends UhcDriver {
 
 	//@FindBy(xpath = "//*[@id='dashboard']//span[text()='View Your Claims']")
 	//@FindBy(xpath = "//*[@id='claims_1']")
-	@FindBy(xpath = "//a[text()='Go to Claims page']")
+//	@FindBy(xpath = "//a[text()='Go to Claims page']")
+	 @FindBy(xpath =".//*[@id='sticky-nav']/sticky-content/nav/div/div/div/div/a[3]")
 	private WebElement claimsDashboardLink;
 
 	@FindBy(xpath = "//*[@id='row2link1']/td[2]/a")
@@ -1178,8 +1179,11 @@ public class AccountHomePage extends UhcDriver {
 				|| (MRScenario.environmentMedicare.equalsIgnoreCase("team-t")
 						|| MRScenario.environment.equalsIgnoreCase("team-ci1"))) {
 			System.out.println("Go to claims link is present "
-					+ driver.findElement(By.xpath("//a[text()='Go to Claims page']")).isDisplayed());
-			driver.findElement(By.xpath("//a[text()='Go to Claims page']")).click();
+					/*+ driver.findElement(By.xpath("//a[text()='Go to Claims page']")).isDisplayed());
+			driver.findElement(By.xpath("//a[text()='Go to Claims page']")).click();*/
+			
+			+  driver.findElement(By.xpath(".//*[@id='sticky-nav']/sticky-content/nav/div/div/div/div/a[3]")).isDisplayed());
+              driver.findElement(By.xpath(".//*[@id='sticky-nav']/sticky-content/nav/div/div/div/div/a[3]")).click(); 
 			return new ClaimSummarypage(driver);
 		} else if (MRScenario.environmentMedicare.equalsIgnoreCase("stage")) {
 			System.out.println("user is on Stage login page");
