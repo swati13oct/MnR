b567ec2224a2cd53a8aee8582c69b6fc503f75aa
Pooja <minhas.pooja@optum.com>
2019-01-29 13:46:52 -0500

    F00000 US 00000

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDtheTransformersTest.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDtheTransformersTest.java
@@ -17,7 +17,7 @@ jsonReport = "target/cucumber-RunMRATDDtheTransformersTest.json",detailedReport 
 	overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDtheTransformersTest")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
 features = { "src/main/resources/feature/memberredesign" }, plugin = {"pretty", "html:reports/test-report", 
-		"json:target/cucumber-RunMRATDDtheTransformersTest.json" }, tags = {" @preEffective2"})
+		"json:target/cucumber-RunMRATDDtheTransformersTest.json" }, tags = {" @claims6"})
 public class RunMRATDDtheTransformersTest {
 
 }
