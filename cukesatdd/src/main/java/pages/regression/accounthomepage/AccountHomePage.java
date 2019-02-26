729532760b1ae8b9574878078c0ae31b00af4a47
Choi, Li-Yan <li-yan.choi@optum.com>
2019-02-26 13:51:43 -0500

    F000000:US000000-update regression scripts for OrderMaterials feature to be able to run in team env

--- cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
@@ -1574,8 +1574,14 @@ public class AccountHomePage extends UhcDriver {
 			executor.executeScript("arguments[0].click();", OrderMaterial_Dashboard);
 			// OrderMaterial_Dashboard.click();
 		} else {
-			String Page_URL = "https://" + MRScenario.environment
-					+ "-medicare."+MRScenario.domain+"//member/order-plan-materials.html";
+			String Page_URL;
+			if (MRScenario.environment.equalsIgnoreCase("team-a")) {
+				Page_URL = "https://www." + MRScenario.environment
+						+ "-medicare."+MRScenario.domain+"/content/medicare/member/order-materials/overview.html";
+			} else {
+				Page_URL = "https://" + MRScenario.environment
+						+ "-medicare."+MRScenario.domain+"//member/order-plan-materials.html";
+			}
 			// String Page_URL = driver.getCurrentUrl().split(".com")[0];
 			driver.navigate().to(Page_URL);
 			System.out.println("Navigated to Order materials Page URL : " + Page_URL);
