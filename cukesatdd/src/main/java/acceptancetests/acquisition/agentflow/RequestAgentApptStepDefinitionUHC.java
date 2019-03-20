dfbdc9eabc034f6ecfb7fb1fae98e98b09665de1
dgoud10 <dgoud10@LH7U05CG7350LZF.ms.ds.uhc.com>
2019-03-20 17:10:38 +0530

    F0000-US123456:Reverting the MA Agent Appointment flow changes

--- cukesatdd/src/main/java/acceptancetests/acquisition/agentflow/RequestAgentApptStepDefinitionUHC.java
+++ cukesatdd/src/main/java/acceptancetests/acquisition/agentflow/RequestAgentApptStepDefinitionUHC.java
@@ -142,7 +142,7 @@ public class RequestAgentApptStepDefinitionUHC {
 		}
 	}
 	
-	@Then("^user validates the breadcrum on the request appointment with an agent in UHC site$")
+	/*@Then("^user validates the breadcrum on the request appointment with an agent in UHC site$")
 	public void user_validates_the_breadcrum_on_the_request_appointment_with_an_agent_in_UHC_site() throws Throwable {
 		RequestAgentAppointmentPage requestAgentAppointmentPage = (RequestAgentAppointmentPage) getLoginScenario().getBean(PageConstants.REQUEST_AGENT_APPOINTMENT_PAGE);
 		boolean errorMessageValidated = requestAgentAppointmentPage.validateErrorMessages();
@@ -151,6 +151,6 @@ public class RequestAgentApptStepDefinitionUHC {
 		}else{
 			Assert.fail("Error in loading error messages");
 		}
-	}
+	}*/
 	
 }
