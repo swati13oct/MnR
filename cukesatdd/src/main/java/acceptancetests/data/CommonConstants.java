f40a953e87be009fab4a45b4041a45b1bce496e9
rubina <rubina.paul@optum.com>
2019-03-22 18:42:15 -0400

    DevImprovment-update

--- cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
+++ cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
@@ -860,7 +860,7 @@ public class CommonConstants {
 	public static final long TIMEOUT_90 = 90;
 	public static final String SAUCELABS_TUNNEL_IDENTIFIER = "SAUCELABS_TUNNEL_IDENTIFIER";
 	//public static final String SAUCELABS_DEFAULT_TUNNEL = "OptumSharedTunnel-Stg";
-	public static final String SAUCELABS_DEFAULT_TUNNEL = "Optum-Prd";
+	public static final String SAUCELABS_DEFAULT_TUNNEL = "Optum-Stage";
 	public static String MAIN_WINDOW_HANDLE_ACQUISITION = null;
 	
 }
