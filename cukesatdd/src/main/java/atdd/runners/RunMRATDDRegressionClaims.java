e3aa00f174b254713a00f22660571dfb2687653b
lchoi2 <li-yan.choi@optum.com>
2019-04-08 16:57:13 -0400

    DevImprovement-update retry=1 for some regression runners

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDRegressionClaims.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDRegressionClaims.java
@@ -14,7 +14,7 @@ import cucumber.api.CucumberOptions;
  */
 
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
 jsonReport = "target/cucumber-RunMRATDDRegressionClaims.json",detailedReport = true, detailedAggregatedReport = true,
 overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionClaims")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
