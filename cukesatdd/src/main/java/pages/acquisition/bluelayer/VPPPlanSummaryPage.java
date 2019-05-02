8428b3e8a63bfe85656e406704c473f5a7f69ce1
Obul reddy <obul_reddy@optum.com>
2019-05-02 17:21:28 +0530

    Update Dce flow for Plan Compare for UHC

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/VPPPlanSummaryPage.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/VPPPlanSummaryPage.java
@@ -1750,7 +1750,7 @@ public boolean validateAllPlansChecked() {
 			e.printStackTrace();
 		}
 		List<WebElement> allMAPlans = driver
-				.findElements(By.xpath(".//*[@id='plan-list-1']//div[contains(@class,'compare-box')]"));
+				.findElements(By.xpath(".//*[@id='plan-list-1']//div[contains(@class,'compare-box')]//label"));
 
 		if (allMAPlans != null) {
 			for (int i = 0; i < allMAPlans.size(); i++) {
