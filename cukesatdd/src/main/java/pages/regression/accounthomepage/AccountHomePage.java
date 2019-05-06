1b138d5a173f32efbe22f3e51815d588e41bf933
lchoi2 <li-yan.choi@optum.com>
2019-05-06 15:37:50 -0400

    F000000:US000000-update preference related script to handle shadow-root

--- cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
@@ -386,6 +386,9 @@ public class AccountHomePage extends UhcDriver {
 
 	@FindBy(tagName="arcade-footer")
 	private WebElement shadowRootFooter;
+	
+	@FindBy(xpath="//span[contains(@class,'account-info-label')]")
+	private WebElement accountLabel;
 
 	private PageData myAccountHome;
 
@@ -643,7 +646,16 @@ public class AccountHomePage extends UhcDriver {
 				} else {
 					//check to see if link is inside shadow-root element
 					locateAndClickElementWithinShadowRoot(shadowRootHeader, "#dropdown-toggle-2 > span > span:nth-child(2)");	
-					locateAndClickElementWithinShadowRoot(shadowRootHeader, "#dropdown-options-2 > a:nth-child(3) > span");	
+					System.out.println("clicked header menu");
+
+					if (validate(accountLabel) && 
+							(accountLabel.getText().toLowerCase().contains("supplement") 
+							|| accountLabel.getText().toLowerCase().contains("medicare prescription drug"))) {
+						locateAndClickElementWithinShadowRoot(shadowRootHeader, "#dropdown-options-2 > a:nth-child(2) > span");	
+					} else {
+						locateAndClickElementWithinShadowRoot(shadowRootHeader, "#dropdown-options-2 > a:nth-child(3) > span");	
+					}
+					System.out.println("clicked header button");
 				}
 				Thread.sleep(6000);	   
 				System.out.println("title is " + driver.getTitle());
