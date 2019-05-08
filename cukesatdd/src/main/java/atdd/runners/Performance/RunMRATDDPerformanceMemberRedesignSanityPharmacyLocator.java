57485b496ace99bab8ca21516ab7cb068c1c7eac
Aayush Shah <aayush.shah@optum.com>
2019-05-08 15:51:45 -0400

    DevImprovement-Updating the member pharmacy locator flow for performance atdd

--- cukesatdd/src/main/java/atdd/runners/Performance/RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator.java
+++ cukesatdd/src/main/java/atdd/runners/Performance/RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator.java
@@ -13,7 +13,7 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberrdesignVBF" }, features = { "src/main/resources/feature/member-redesign-vbf/pharmacy-locator" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator.json" }, tags = { "@smokeTest_PharmacyLocatorMem" },monochrome = true)
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator.json" }, tags = { "@pharmacyLocator_Performance" },monochrome = true)
 
 public class RunMRATDDPerformanceMemberRedesignSanityPharmacyLocator {
 
