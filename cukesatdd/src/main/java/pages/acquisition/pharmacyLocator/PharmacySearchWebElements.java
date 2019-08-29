92a8153d69c4a763d79a98b895a1f752fbb5b0c9
lchoi2 <li-yan.choi@optum.com>
2019-08-29 19:27:40 -0400

    F279237:US000000-member pharmacy locator scripts - update xpath

--- cukesatdd/src/main/java/pages/acquisition/pharmacyLocator/PharmacySearchWebElements.java
+++ cukesatdd/src/main/java/pages/acquisition/pharmacyLocator/PharmacySearchWebElements.java
@@ -180,7 +180,7 @@ public class PharmacySearchWebElements extends UhcDriver {
 	@FindBy(xpath="//p[contains(text(),'see the pharmacy')]")
 	protected WebElement areaUnderArrow;
 
-	@FindBy(xpath="//a[contains(@href,'contact-us.html')]")
+	@FindBy(xpath="//div[contains(@ng-show,'pharmacyModel') and not(contains(@class,'ng-hide'))]//a[contains(@href,'contact-us.html')]")
 	protected WebElement contactUnitedHealthCare;
 
 	@FindBy(xpath="//div[not(contains(@class,'ng-hide'))]/p/a[contains(@href,'LTC_HI_ITU_Pharmacies_Other.pdf')]")
