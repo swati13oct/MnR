aa4d06572ea0c4bf494fb14433d049da822d0910
lchoi2 <li-yan.choi@optum.com>
2019-11-15 17:07:05 -0500

    F355345:US000000-update PnP regression for link validation on acct setting pg

--- cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
+++ cukesatdd/src/main/java/pages/regression/accounthomepage/AccountHomePage.java
@@ -3283,6 +3283,7 @@ public class AccountHomePage extends UhcDriver {
 		String secondTopMenuItemCssStr="#main-nav > div > div > div > a:nth-child(2)";
 		WebElement secondTopMenuItem = locateElementWithinShadowRootNoAssert(shadowRootHeader,
 				secondTopMenuItemCssStr);
+		Assert.assertTrue("PROBLEM - unable locate top menu item", secondTopMenuItemCssStr!=null);
 		if (secondTopMenuItem!=null && secondTopMenuItem.getText().contains("FIND CARE")) {
 			String pnpTopMenuItemCssStr="#main-nav > div > div > div > a:nth-child(6)";
 			WebElement pnpTopMenuLink = locateElementWithinShadowRootNoAssert(shadowRootHeader,
