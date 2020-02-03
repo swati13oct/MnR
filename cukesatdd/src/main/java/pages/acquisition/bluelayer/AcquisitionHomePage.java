bc62b033de4a5ed9faa4846ce2515b3e35b30161
Aayush Shah <aayush.shah@optum.com>
2020-02-03 11:09:39 -0500

    DevImprovement-Reverting changes for mrscenario and acquisition home page

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/AcquisitionHomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/AcquisitionHomePage.java
@@ -335,9 +335,6 @@ public class AcquisitionHomePage extends GlobalWebElements {
 	private static String UMS_ACQISITION_OFFLINE_PAGE_URL = MRConstants.UHC_URL_OFFLINE;
 	private static String UMS_ACQISITION_PROD_PAGE_URL = MRConstants.UHCM_URL_PROD;
 	private static String AARP_ACQISITION_PROD_PAGE_URL = MRConstants.AARP_URL_PROD;	
-	private static String Local_AARP_URL = MRConstants.Local_URL;	
-	private static String DIGITAL_AARP_URL = MRConstants.TeamDigital_AARP_URL;
-	private static String DIGITAL_UHC_URL = MRConstants.TeamDigital_UHC_URL;
 	private static String AARP_ACQISITION_PAGE_URL = MRConstants.AARP_URL;
 	private static String AARP_ACQISITION_OFFLINE_PAGE_URL = MRConstants.AARP_URL_OFFLINE;
 
@@ -428,12 +425,6 @@ public class AcquisitionHomePage extends GlobalWebElements {
 		}else if (MRScenario.environment.equals("prod")) {
 			startNew(UMS_ACQISITION_PROD_PAGE_URL);
 			checkModelPopup(driver,45);
-		} else if(MRScenario.environment.equals("local")){
-			startNew(Local_AARP_URL);
-		} else if(MRScenario.environmentName.equalsIgnoreCase("TEAMDIGITAL_AARP_URL")){
-			startNew(DIGITAL_AARP_URL);
-		} else if(MRScenario.environmentName.equalsIgnoreCase("TEAMDIGITAL_UHC_URL")){
-			startNew(DIGITAL_UHC_URL);
 		}else {
 			startNew(UMS_ACQISITION_PAGE_URL);
 			checkModelPopup(driver,10);
