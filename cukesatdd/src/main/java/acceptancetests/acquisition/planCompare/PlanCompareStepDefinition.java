4d51b8ebd37dd6906897d9bdfe3b3c10aa385d44
Obul reddy <obul_reddy@optum.com>
2020-02-10 14:04:31 +0530

    Added packages for plan compare pages

---
+++ cukesatdd/src/main/java/acceptancetests/acquisition/planCompare/PlanCompareStepDefinition.java
@@ -0,0 +1,43 @@
+package acceptancetests.acquisition.planCompare;
+
+import gherkin.formatter.model.DataTableRow;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import acceptancetests.data.CommonConstants;
+import acceptancetests.data.PageConstants;
+import acceptancetests.data.PageConstantsMnR;
+import atdd.framework.MRScenario;
+import cucumber.api.DataTable;
+import cucumber.api.Scenario;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ *Functionality:PharmacyLocator
+ */
+public class PlanCompareStepDefinition {
+
+	@Autowired
+	MRScenario loginScenario;
+	String langName;
+
+	public MRScenario getLoginScenario() {
+		return loginScenario;
+	}
+
+	
+}
+
+
