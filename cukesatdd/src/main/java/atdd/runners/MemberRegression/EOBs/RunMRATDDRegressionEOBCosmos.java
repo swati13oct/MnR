533a67a100120354375820c675c7b28f7f156077
Sneha Dwarakanath <sneha.dwarakanath@optum.com>
2020-02-21 23:45:50 -0500

    F00000:US000000 - Separated Runners for EOB types

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/EOBs/RunMRATDDRegressionEOBCosmos.java
@@ -0,0 +1,25 @@
+package atdd.runners.MemberRegression;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionEOBCosmos.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionEOBCosmos")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
+features = { "src/main/resources/feature/memberredesign/eob" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionEOBCosmos.json" }, tags = { " @COSMOS_EOBs" })
+public class RunMRATDDRegressionEOBCosmos {
+
+}
