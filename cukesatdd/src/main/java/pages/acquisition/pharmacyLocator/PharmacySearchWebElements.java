d777bb6b02e3d6ed6888b16a2f2ffeb4b339e54f
Aayush Shah <aayush.shah@optum.com>
2020-03-12 19:09:34 -0400

    DevImprovement-Pharmacy Locator flow xpath changed

--- cukesatdd/src/main/java/pages/acquisition/pharmacyLocator/PharmacySearchWebElements.java
+++ cukesatdd/src/main/java/pages/acquisition/pharmacyLocator/PharmacySearchWebElements.java
@@ -156,7 +156,7 @@ public class PharmacySearchWebElements extends UhcDriver {
 	/*@FindBy(id="indian-tribal-label")
 	protected WebElement indian_tribal_label_filter;*/
 	
-	@FindBy(xpath="//label[@id='Indian/Tribal/Urban')]")
+	@FindBy(xpath="//*[contains(@id,'indian-tribal-label')]")
 	protected WebElement indian_tribal_label_filter;
 
 	@FindBy (id= "noResultsFoundErrorMessage")
