753d84e8f73378483a0834195757969becde3a67
Murugasamy <murali.murugasamy@optum.com>
2020-03-16 16:00:24 +0530

    update

--- cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionPlanRecommendationEngine.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionPlanRecommendationEngine.java
@@ -25,7 +25,7 @@ import cucumber.api.CucumberOptions;
 		glue = { "atdd.framework", "acceptancetests.acquisition.planRecommendationEngine" }, 
 		features = { "src/main/resources/feature/acquisition/PlanRecommendationEngine" }, 
 		plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionPlanRecommendationEngine.json" }, 
-		tags = {"@F372735"})
+		tags = {"@F372736"})
 public class RunMRATDDAcquisitionPlanRecommendationEngine {
 
 }
