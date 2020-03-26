ec8146cfa13c5eb43687913e1abc0d810917c034
lchoi2 <li-yan.choi@optum.com>
2020-03-25 21:09:59 -0400

    F411758:US000000-update eob regression scripts

--- cukesatdd/src/main/java/acceptancetests/memberredesign/expalnationofbenefits/EobStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/memberredesign/expalnationofbenefits/EobStepDefinition.java
@@ -424,6 +424,7 @@ public class EobStepDefinition {
 		int ui_eobResultCount=eobPage.getNumEobAfterSearch();
 		getLoginScenario().saveBean(EobCommonConstants.UI_EOB_COUNT, ui_eobResultCount);
 		
+		/* keep - re-active validation when download API is stable
 		if (ui_eobResultCount>0) {
 			EobApiResponse eobApiResponse=(EobApiResponse) getLoginScenario().getBean(EobCommonConstants.API_EOB_RESPONSE);
 			//TODO comment out for now b/c current UI not working as copy deck version
@@ -431,7 +432,7 @@ public class EobStepDefinition {
 			eobPage.validateEOBStatements_dream(ui_eobResultCount, eobApiResponse);
 		} else {
 			System.out.println("TEST - EOB has no EOB for this search period, skip the text validation");
-		}
+		} */
 	}
 
 	@And("^the user clicks on first eob from the list to validate pdf$") //keep
