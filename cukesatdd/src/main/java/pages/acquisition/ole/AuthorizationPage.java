27232ebb40a91731ae3a1f71baed8109dd020c81
Aayush Shah <aayush.shah@optum.com>
2020-03-27 15:57:21 -0400

    DevImprovement-OLE flow updated

--- cukesatdd/src/main/java/pages/acquisition/ole/AuthorizationPage.java
+++ cukesatdd/src/main/java/pages/acquisition/ole/AuthorizationPage.java
@@ -112,7 +112,7 @@ public class AuthorizationPage extends UhcDriver{
 	@Override
 	public void openAndValidate() {
 		CommonUtility.waitForPageLoadNew(driver, PageHeader,30);
-		if(PageHeader.getText().contains("Authorizations"))
+		if(PageHeader.getText().contains("Authorization"))
 			System.out.println("Page header is Displayed : "+PageHeader.getText());
 		else
 			Assert.fail("Error in validating the Authorization page loaded");
