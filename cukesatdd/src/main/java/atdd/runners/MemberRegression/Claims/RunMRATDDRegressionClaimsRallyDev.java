5bb6d8215bd3201c5db32d3e84095344f715499b
lchoi2 <li-yan.choi@optum.com>
2020-04-02 13:40:33 -0400

    F411758:US000000-update claims regression scripts

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/Claims/RunMRATDDRegressionClaimsRallyDev.java
@@ -0,0 +1,19 @@
+package atdd.runners.MemberRegression.Claims;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionClaimsRallyDev.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionClaimsRallyDev")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
+features = { "src/main/resources/feature/memberredesign/claims" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionClaimsRallyDev.json" }, tags = { "@rallyClaims01","@devRegression" })
+public class RunMRATDDRegressionClaimsRallyDev {
+//note: use this runner for drug claims
+}
