43a714ed4451bcb6973a8900ef142898b8d44cef
Obul reddy <obul_reddy@optum.com>
2020-04-14 18:10:48 +0530

    updated Runner file for testharness vp

--- cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionVisitorProfileTestharnessAARP.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionVisitorProfileTestharnessAARP.java
@@ -14,7 +14,7 @@ import cucumber.api.CucumberOptions;
 @ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionVisitorProfileTestharnessAARP.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionVisitorProfileTestharnessAARP")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/visitorProfile" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionVisitorProfileTestharnessAARP.json" }, tags = { "@vpTestharnessAARP04" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionVisitorProfileTestharnessAARP.json" }, tags = { "@vpMicroAppAARP" })
 public class RunMRATDDAcquisitionVisitorProfileTestharnessAARP { 
  
 }
