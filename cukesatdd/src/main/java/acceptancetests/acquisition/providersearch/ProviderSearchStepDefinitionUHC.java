f45dcf15e67492db9175981257b04b7e532a7447
Paul, Rubina <rubina.paul@optum.com>
2020-04-27 16:57:05 -0400

    DevImprovement-update

--- cukesatdd/src/main/java/acceptancetests/acquisition/providersearch/ProviderSearchStepDefinitionUHC.java
+++ cukesatdd/src/main/java/acceptancetests/acquisition/providersearch/ProviderSearchStepDefinitionUHC.java
@@ -25,7 +25,7 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 /**
- * Functionality:ProviderSearch
+ * Functionality:ProviderSearch 
  */
 
 public class ProviderSearchStepDefinitionUHC {
