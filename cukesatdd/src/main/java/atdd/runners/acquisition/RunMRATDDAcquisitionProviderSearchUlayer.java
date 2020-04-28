8fa322c9cb0c555286ef284fc2545b5cc29175ba
Paul, Rubina <rubina.paul@optum.com>
2020-04-28 13:12:54 -0400

    DevImprovement-update

--- cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionProviderSearchUlayer.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionProviderSearchUlayer.java
@@ -14,7 +14,7 @@ import cucumber.api.CucumberOptions;
 @ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionProviderSearchUlayer.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionProviderSearchUlayer")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/providersearch" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionProviderSearchUlayer.json" }, tags = { "@ProviderSearchFromWidgetUlayer" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionProviderSearchUlayer.json" }, tags = { "@AcqRegressionProviderSearchUlayer" })
 public class RunMRATDDAcquisitionProviderSearchUlayer{ 
  
 }
