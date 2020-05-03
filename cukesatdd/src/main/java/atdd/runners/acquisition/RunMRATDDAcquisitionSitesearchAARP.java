7e9414c6d3e6f3dde92317e603d799cf728504dd
sarvind1 <sarvind1@vetsvra00007676.ms.ds.uhc.com>
2020-05-03 21:28:27 +0530

    commit - sitesearch functionality

---
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionSitesearchAARP.java
@@ -0,0 +1,20 @@
+package atdd.runners.acquisition;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * this file is to run test cases for RunMRATDDAcquisitionVPPDCE
+ */
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionSitesearchAARP.json",detailedReport = true,
+ detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionSitesearchAARP")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/Sitesearch" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionSitesearchAARP.json" }, tags = { "@SiteSearchRegressionAARP" })
+public class RunMRATDDAcquisitionSitesearchAARP { 
+ 
+}
