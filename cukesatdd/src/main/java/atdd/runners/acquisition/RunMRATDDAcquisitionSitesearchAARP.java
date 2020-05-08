d2e164c8d1f0af8f1a4cd0b907e5992994958be7
Aayush Shah <aayush.shah@optum.com>
2020-05-08 15:59:02 -0400

    DevImprovement-Increasing retry count

--- cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionSitesearchAARP.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionSitesearchAARP.java
@@ -11,7 +11,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionVPPDCE
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionSitesearchAARP.json",detailedReport = true,
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionSitesearchAARP.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionSitesearchAARP")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/Sitesearch/SitesearchAARP.feature" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionSitesearchAARP.json" }, tags = { "@SiteSearchResultsAARP" })
