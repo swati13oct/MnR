0f2d977431f45ab09e3dce13f4ab08aa14788fb4
ujethwa <umang.jethwa@optum.com>
2020-05-18 16:06:11 -0500

    F360264:US000000 Footer Runner Files feature files added for prod

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/Footer/RunMRATDDRegressionFooterMemAuthProd.java
@@ -0,0 +1,25 @@
+package atdd.runners.MemberRegression.Footer;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ *
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+        jsonReport = "target/cucumber-RunMRATDDRegressionFooterMemAuthProd.json",detailedReport = true, detailedAggregatedReport = true,
+        overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionFooterMemAuthProd")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, monochrome = true,
+        features = { "src/main/resources/feature/memberredesign/footer" }, plugin = {
+        "pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionFooterMemAuthProd.json" }, tags = { "@prod_footer01","@prod_footer02","@prod_footer03","@prod_footer04" })
+public class RunMRATDDRegressionFooterMemAuthProd {
+
+}
