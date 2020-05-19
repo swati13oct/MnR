a3264f25a35f87090f0da19c8b62111640f501e0
akapoo18 <alisha_kapoor@optum.com>
2020-05-19 15:39:43 +0530

    F360264:US000000 Fixed footer ATDD

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/Footer/RunMRATDDRegressionFooterMemAuthProd.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/Footer/RunMRATDDRegressionFooterMemAuthProd.java
@@ -19,7 +19,7 @@ import cucumber.api.CucumberOptions;
         overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionFooterMemAuthProd")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, monochrome = true,
         features = { "src/main/resources/feature/memberredesign/footer" }, plugin = {
-        "pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionFooterMemAuthProd.json" }, tags = { "@prod_footer01","@prod_footer02","@prod_footer03","@prod_footer04" })
+        "pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionFooterMemAuthProd.json" }, tags = { "@prod_footer01,@prod_footer02,@prod_footer03,@prod_footer04" })
 public class RunMRATDDRegressionFooterMemAuthProd {
 
 }
