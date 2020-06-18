a072e61a8d9f6849ba50f4b4eb102c6a0007c607
lchoi2 <li-yan.choi@optum.com>
2020-06-18 14:26:40 -0400

    F411758:US000000-split planDoc runners

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/PlanDocAndRes/OfflineProdOrOnlineProd/RunMRATDDRegressionPlanDocAndResMemAuthProd_P6_ActMaGrp_P1of7.java
@@ -0,0 +1,24 @@
+package atdd.runners.MemberRegression.PlanDocAndRes.OfflineProdOrOnlineProd;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionPlanDocAndResMemAuthProd_P6_ActMaGrp_P1of7.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionPlanDocAndResMemAuthProd_P6_ActMaGrp_P1of7")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" },
+features = { "src/main/resources/feature/memberredesign/planDocumentsAndResources" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionPlanDocAndResMemAuthProd_P6_ActMaGrp_P1of7.json" }, tags = { "@prod_planAndDocuments06_1of7","@prod_active_ma_grp_1of7" })
+public class RunMRATDDRegressionPlanDocAndResMemAuthProd_P6_ActMaGrp_P1of7 {
+}
