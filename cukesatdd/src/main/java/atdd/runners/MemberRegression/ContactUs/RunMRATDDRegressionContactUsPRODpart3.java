adea57dc87fb7e5c756451c0a83b2fd83c6aedd2
Gupta, Kapil <kapil_gupta44@optum.com>
2020-06-19 14:31:45 +0530

    contactUS runner updated

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/ContactUs/RunMRATDDRegressionContactUsPRODpart3.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/ContactUs/RunMRATDDRegressionContactUsPRODpart3.java
@@ -15,11 +15,11 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
-jsonReport = "target/cucumber-RunMRATDDRegressionContactUsPROD.json",detailedReport = true, detailedAggregatedReport = true,
-overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionContactUsPROD")
+jsonReport = "target/cucumber-RunMRATDDRegressionContactUsPRODpart3.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionContactUsPRODpart3")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, monochrome=true,
 features = { "src/main/resources/feature/memberredesign/contactus/ContactUsPROD.feature" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionContactUsPROD.json" }, tags = { "@regressionMemberPROD3" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionContactUsPRODpart3.json" }, tags = { "@regressionMemberPROD3" })
 public class RunMRATDDRegressionContactUsPRODpart3 {
 
 }
