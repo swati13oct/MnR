f450c3aaaf46364acfdf243f3fdd59a58404cd21
venkata naga sai kumar kanagala <venkata.kanagala@optum.com>
2020-06-20 15:24:25 -0500

    updated

--- cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionOLEUlayerPCP1.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionOLEUlayerPCP1.java
@@ -12,7 +12,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionOLEUlayer
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionOLEUlayerPCP.json",detailedReport = true,
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionOLEUlayerPCP.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionOLEUlayerPCP")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },monochrome=true, features = { "src/main/resources/feature/acquisition/ole" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionOLEUlayerPCP.json" }, tags = { "@MA_OLE_AARP_LT" })
