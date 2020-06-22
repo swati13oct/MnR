cd2d3b24be505d0d6c7b53e36944bb112c43991f
Aayush Shah <aayush.shah@optum.com>
2020-06-22 18:15:47 -0400

    DevImprovement-Change row

--- cukesatdd/src/main/java/acceptancetests/acquisition/vpp/VppPlanValidationStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/acquisition/vpp/VppPlanValidationStepDefinition.java
@@ -131,7 +131,7 @@ public class VppPlanValidationStepDefinition {
 				 
 				 HashMap <String, String> benefitsMap = new HashMap<String, String>();
 				 //Looping over total rows with values
-				 for(int rowIndex=0; rowIndex<=lastRow; rowIndex++)
+				 for(int rowIndex=0; rowIndex<=1; rowIndex++)
 		            {
 					 	int cellIndex = 0;System.out.println("INSIDE Row");
 					 	
