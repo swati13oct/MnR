b62fe560bbb7c68e8404d47c5f070634f4760656
lchoi2 <li-yan.choi@optum.com>
2020-06-26 23:03:28 -0400

    F411758:US000000-update regression scripts

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/HealthAndWellness/RunMRATDDRegressionHealthAndWellnessMemAuthProdP1.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/HealthAndWellness/RunMRATDDRegressionHealthAndWellnessMemAuthProdP1.java
@@ -15,7 +15,7 @@ import cucumber.api.CucumberOptions;
  */
 
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
 jsonReport = "target/cucumber-RunMRATDDRegressionHealthAndWellnessMemAuthProdP1.json",detailedReport = true, detailedAggregatedReport = true,
 overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionHealthAndWellnessMemAuthProdP1")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
