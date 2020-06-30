aba5a93f63d925aa3fee96cc5325ebf88544b391
kkumard <kkumard@LDTU000025DJ3Z2.ms.ds.uhc.com>
2020-06-30 14:17:32 -0500

    New runners for Benefits.

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/BenefitNCoverage/RunMRATDDRegressionBenefitNCoverage_LinksValidation_SSUP_MemAuthProd.java
@@ -0,0 +1,25 @@
+package atdd.runners.MemberRegression.BenefitNCoverage;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionBenefitNCoverage_LinksValidation_SSUP_MemAuthProd.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionBenefitNCoverage_LinksValidation_SSUP_MemAuthProd")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
+features = { "src/main/resources/feature/memberredesign/benefitsandcoverage" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionBenefitNCoverage_LinksValidation_SSUP_MemAuthProd.json" }, tags = {"@Links_Validation_SSUP"})
+public class RunMRATDDRegressionBenefitNCoverage_LinksValidation_SSUP_MemAuthProd {
+
+}
---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/BenefitNCoverage/RunMRATDDRegressionBenefitNCoverage_Pdf_Verification_Lis_MemAuthProd.java
@@ -0,0 +1,25 @@
+package atdd.runners.MemberRegression.BenefitNCoverage;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * @author schak38
+ *
+ */
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionBenefitNCoverage_Pdf_Verification_Lis_MemAuthProd.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionBenefitNCoverage_Pdf_Verification_Lis_MemAuthProd")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
+features = { "src/main/resources/feature/memberredesign/benefitsandcoverage" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionBenefitNCoverage_Pdf_Verification_Lis_MemAuthProd.json" }, tags = {"@pdf_Verification_Lis_Mapd"})
+public class RunMRATDDRegressionBenefitNCoverage_Pdf_Verification_Lis_MemAuthProd {
+
+}
