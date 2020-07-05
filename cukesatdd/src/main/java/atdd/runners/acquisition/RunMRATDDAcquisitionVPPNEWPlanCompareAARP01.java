123feaf2597540190a85c20bb561718f9b8e596c
Obul reddy <obul_reddy@optum.com>
2020-07-05 14:50:35 +0530

    Added new runner files for plan compare

---
+++ cukesatdd/src/main/java/atdd/runners/acquisition/RunMRATDDAcquisitionVPPNEWPlanCompareAARP01.java
@@ -0,0 +1,21 @@
+package atdd.runners.acquisition;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * 
+ * this file is to run test cases for RunMRATDDAcquisitionVppPlanSummary
+ */
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionVPPNEWPlanCompareAARP01.json",detailedReport = true,
+ detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionVPPNEWPlanCompareAARP01")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/vpp/MicroAPP/" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionVPPNEWPlanCompareAARP01.json" }, tags = { "@vppPlanCompareAARPRun01New" })
+public class RunMRATDDAcquisitionVPPNEWPlanCompareAARP01 { 
+ 
+}
