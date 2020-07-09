d7bf60a68ded16762c6e5d72cc4954edde431566
venkata naga sai kumar kanagala <venkata.kanagala@optum.com>
2020-07-09 07:53:29 -0500

    Updated for Campaign TFN

--- cukesatdd/src/main/java/pages/acquisition/tfn/CampaignTFNPage.java
+++ cukesatdd/src/main/java/pages/acquisition/tfn/CampaignTFNPage.java
@@ -95,8 +95,8 @@ public class CampaignTFNPage extends UhcDriver {
 	@FindBy(xpath = "//a[contains(text(),'AARP Medicare Plans from UnitedHealthcare')]")
 	public WebElement AARPSearchLinkfromBing;
 
-	//@FindBy(xpath = "//a[contains(@href,'https://www.uhcmedicaresolutions.com/health-plans/shop/medicare-advantage-plans.html')]")
-	@FindBy(xpath = "//h2//a[contains(text(),'Find Medicare Plans ')]")
+	@FindBy(xpath = "//a[contains(@href,'https://www.uhcmedicaresolutions.com/health-plans/shop/medicare-advantage-plans.html')]")
+	//@FindBy(xpath = "//h2//a[contains(text(),'Find Medicare Plans ')]")
 	public WebElement UHCSearchLinkfromBing;
 
 
