dcf12101f921a7f12d24c987489d9c510356f879
ntalesha <ntalesha@vetsvra00008529.ms.ds.uhc.com>
2020-08-04 16:18:36 +0530

    Dev Improvements-Added Runner file for RefillRemaining and RefillMedicationCTA

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/PharmaciesAndPrescriptions/RunMRATDDRegressionRefillsRemaining.java
@@ -0,0 +1,19 @@
+package atdd.runners.MemberRegression.PharmaciesAndPrescriptions;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionRefillsRemaining.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionRefillsRemaining")
+@CucumberOptions(
+glue = { "atdd.framework", "acceptancetests.memberredesign" },
+features = { "src/main/resources/feature/memberredesign/pharmaciesandprescriptions" },
+plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionRefillsRemaining.json" },
+tags = { "@F479509" }, monochrome =true)
+public class RunMRATDDRegressionRefillsRemaining {
+
+}
