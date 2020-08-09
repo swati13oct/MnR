120da8a32422463493bc4be021f5b21b81bf130d
yaihemai <yusufu.aihemaitijiang@optum.com>
2020-08-09 14:53:25 -0500

    DevImprovement-fixed need help tag

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/PharmaciesAndPrescriptions/RunMRATDDRegressionNeedHelp.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/PharmaciesAndPrescriptions/RunMRATDDRegressionNeedHelp.java
@@ -11,7 +11,7 @@ jsonReport = "target/cucumber-RunMRATDDRegressionNeedHelp.json",detailedReport =
 overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionNeedHelp")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
 features = { "src/main/resources/feature/memberredesign/pharmaciesandprescriptions" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionNeedHelp.json" }, tags = { "@@needHelppp" }, monochrome=true)
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionNeedHelp.json" }, tags = { "@needHelppp" }, monochrome=true)
 public class RunMRATDDRegressionNeedHelp {
 
 }
