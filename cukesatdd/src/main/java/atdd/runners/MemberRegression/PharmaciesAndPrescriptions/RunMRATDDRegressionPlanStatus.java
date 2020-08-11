183d70bf56664c17f299a33d0a6f8f7fa3922bb7
Yusufu Aihemaitijiang <yusufu.aihemaitijiang@optum.com>
2020-08-11 16:31:49 -0500

    DevImprovements: fixed Plan status

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/PharmaciesAndPrescriptions/RunMRATDDRegressionPlanStatus.java
@@ -0,0 +1,17 @@
+package atdd.runners.MemberRegression.PharmaciesAndPrescriptions;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+		jsonReport = "target/cucumber-RunMRATDDRegressionPlanStatus.json",detailedReport = true, detailedAggregatedReport = true,
+		overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionPlanStatus")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" },
+		features = { "src/main/resources/feature/memberredesign/pharmaciesandprescriptions" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionPlanStatus.json" }, tags = { "@F480452" }, monochrome=true)
+public class RunMRATDDRegressionPlanStatus {
+
+}
