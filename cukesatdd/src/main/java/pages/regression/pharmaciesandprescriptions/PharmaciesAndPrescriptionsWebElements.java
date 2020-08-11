ea1d2b4f5c397fab4f72bdef0dbb678efbca7ef3
Talesha <talesha_naresh@optum.com>
2020-08-11 17:41:01 +0530

    Dev Improvements-Adding Feature file For Sprint 8

--- cukesatdd/src/main/java/pages/regression/pharmaciesandprescriptions/PharmaciesAndPrescriptionsWebElements.java
+++ cukesatdd/src/main/java/pages/regression/pharmaciesandprescriptions/PharmaciesAndPrescriptionsWebElements.java
@@ -1106,7 +1106,7 @@ public class PharmaciesAndPrescriptionsWebElements extends UhcDriver {
 	@FindBy(xpath="//a[@data-testid='medication-action-refill']/ancestor::div[@data-testid]//div[@data-testid='medication-data-pharmacy-name']")
 	protected List<WebElement> listOfPharmacyEligibleFrRefill;
 
-	@FindBy(xpath = "//button[@data-testid='medication-action-renew']/ancestor::div[@data-testid]//div[@data-testid='medication-data-pharmacy-name']")
+	@FindBy(xpath = "//a[@data-testid='medication-action-renew']/ancestor::div[@data-testid]//div[@data-testid='medication-data-pharmacy-name']")
 	protected List<WebElement> listOfPharmacyEligibleFrRenew;
 
 	@FindBy(xpath = "//button[@data-testid='medication-action-resolve-hold']/ancestor::div[@data-testid]//div[@data-testid='medication-data-pharmacy-name']")
