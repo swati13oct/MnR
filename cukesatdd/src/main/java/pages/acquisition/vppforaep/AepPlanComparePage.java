47d2d28cbf9790184e210e056d252a842d13d1c4
gbandar2 <gruhalakshmi_bandaru@optum.com>
2020-08-24 20:16:45 -0500

    Changing validate method to validateNew method

--- cukesatdd/src/main/java/pages/acquisition/vppforaep/AepPlanComparePage.java
+++ cukesatdd/src/main/java/pages/acquisition/vppforaep/AepPlanComparePage.java
@@ -157,7 +157,7 @@ public class AepPlanComparePage extends UhcDriver {
 
 		HashMap<String, String> result=new HashMap<String, String>();
 
-		validate(driver.findElement(By.id(tableId)),5);
+		validateNew(driver.findElement(By.id(tableId)));
 
 		String rowXpath="//table[contains(@id,'"+tableId+"')]//tbody//tr[contains(@class,'uhc')]";
 		List<WebElement> listOfRowsInPlanCompareTbl = driver.findElements(By.xpath(rowXpath));
