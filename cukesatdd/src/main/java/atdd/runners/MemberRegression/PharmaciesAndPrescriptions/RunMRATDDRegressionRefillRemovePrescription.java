1b88a435608d6ed25503cc386a26f19b42a28766
Yusufu Aihemaitijiang <yusufu.aihemaitijiang@optum.com>
2020-08-31 16:28:14 -0500

    DevImprovements: created runner for refill remove prescription

---
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/PharmaciesAndPrescriptions/RunMRATDDRegressionRefillRemovePrescription.java
@@ -0,0 +1,19 @@
+package atdd.runners.MemberRegression.PharmaciesAndPrescriptions;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDRegressionRefillRemovePrescription.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDRegressionRefillRemovePrescription")
+@CucumberOptions(
+glue = { "atdd.framework", "acceptancetests.memberredesign" },
+features = { "src/main/resources/feature/memberredesign/pharmaciesandprescriptions" },
+plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDRegressionRefillRemovePrescription.json" },
+tags = { "@F482460" }, monochrome =true)
+public class RunMRATDDRegressionRefillRemovePrescription {
+
+}
