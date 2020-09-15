8e5f19567e2e34e83e412b810946ac5e23516223
Rathulya <rathulya_ravindran@optum.com>
2020-09-15 19:43:58 +0530

    updated runnner

--- cukesatdd/src/main/java/atdd/runners/acquisition/DCERedesign/RunMRATDDAcqDCEDrugSummaryPageAARP.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/DCERedesign/RunMRATDDAcqDCEDrugSummaryPageAARP.java
@@ -12,7 +12,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionDCERegressionUlayerHome - Scenario for VPP Page
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcqDCEDrugSummaryPageAARP.json",detailedReport = true,
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcqDCEDrugSummaryPageAARP.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcqDCEDrugSummaryPageAARP")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcqDCEDrugSummaryPageAARP.json" }, tags = { "@dceRedesignExtraHelpAlertDetailPage" })
