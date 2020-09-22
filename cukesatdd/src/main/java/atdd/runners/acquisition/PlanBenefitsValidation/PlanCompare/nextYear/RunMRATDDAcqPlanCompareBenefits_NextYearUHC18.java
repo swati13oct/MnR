61579cab178ddf69ce59cf06266169b8f977064f
gbandar2 <gruhalakshmi_bandaru@optum.com>
2020-09-22 05:35:00 -0500

    Adding more runner files for 2021 plans

---
+++ cukesatdd/src/main/java/atdd/runners/acquisition/PlanBenefitsValidation/PlanCompare/nextYear/RunMRATDDAcqPlanCompareBenefits_NextYearUHC18.java
@@ -0,0 +1,30 @@
+package atdd.runners.acquisition.PlanBenefitsValidation.PlanCompare.nextYear;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import org.junit.runner.RunWith;
+
+/**
+ * this file is to run plan benefits validations on plan Compare page
+ */
+@RunWith(ExtendedCucumber.class)
+
+@ExtendedCucumberOptions(
+        retryCount=0,
+        screenShotSize="",
+        screenShotLocation="/screenshots/",
+        jsonReport = "target/cucumber-RunMRATDDAcqPlanCompareBenefits_NextYearUHC18.json",
+        detailedReport = true,
+        detailedAggregatedReport = true,
+        overviewReport = true,
+        toPDF = true,
+        outputFolder = "target/RunMRATDDAcqPlanCompareBenefits_NextYearUHC18")
+@CucumberOptions(
+        glue = { "atdd.framework", "acceptancetests.acquisition.vpp" },
+        monochrome = true,
+        features = { "src/main/resources/feature/acquisition/vpp" },
+        plugin = { "pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcqPlanCompareBenefits_NextYearUHC18.json" },
+        tags = { "@next_year","@planCompareBenefits_NextYearUHC18" })
+public class RunMRATDDAcqPlanCompareBenefits_NextYearUHC18 {
+}
