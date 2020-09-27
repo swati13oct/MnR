181d43f351126f7dd16b2f48cd3468e056bc65db
hahire <hahire@vetsvra00011242.ms.ds.uhc.com>
2020-09-27 12:45:39 +0530

    RetryCont set zero

--- cukesatdd/src/main/java/atdd/runners/mobile/acquisition/vpp/RunMRATDDAcquisitionVPPPlanCompareMobile.java
+++ cukesatdd/src/main/java/atdd/runners/mobile/acquisition/vpp/RunMRATDDAcquisitionVPPPlanCompareMobile.java
@@ -11,7 +11,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionVppPlanCompare
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
 jsonReport = "target/cucumber-RunMRATDDAcquisitionVppPlanCompare.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, 
  outputFolder = "target/RunMRATDDAcquisitionVppPlanCompare")
