12cee8df341e07b178ef19a101318ee18028b6d6
kkumard <kkumard@LDTU000025DJ3Z2.ms.ds.uhc.com>
2020-10-02 14:38:58 -0500

    updated

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/BenefitNCoverage/RunMRATDDSanity_pdpIndividual_lis_BenefitNCoverage_PartMemAuthProd.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/BenefitNCoverage/RunMRATDDSanity_pdpIndividual_lis_BenefitNCoverage_PartMemAuthProd.java
@@ -15,11 +15,11 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
-jsonReport = "target/cucumber-RunMRATDDSanity_pdp_lis_BenefitNCoverage_PartMemAuthProd.json",detailedReport = true, detailedAggregatedReport = true,
-overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDSanity_pdp_lis_BenefitNCoverage_PartMemAuthProd")
+jsonReport = "target/cucumber-RunMRATDDSanity_pdpIndividual_lis_BenefitNCoverage_PartMemAuthProd.json",detailedReport = true, detailedAggregatedReport = true,
+overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDSanity_pdpIndividual_lis_BenefitNCoverage_PartMemAuthProd")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
 features = { "src/main/resources/feature/memberredesign/benefitsandcoverage" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDSanity_pdp_lis_BenefitNCoverage_PartMemAuthProd.json" }, tags = {"@bnc_sanity_pdp"})
-public class RunMRATDDSanity_pdp_lis_BenefitNCoverage_PartMemAuthProd {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDSanity_pdpIndividual_lis_BenefitNCoverage_PartMemAuthProd.json" }, tags = {"@bnc_sanity_pdp"})
+public class RunMRATDDSanity_pdpIndividual_lis_BenefitNCoverage_PartMemAuthProd {
 
 }
