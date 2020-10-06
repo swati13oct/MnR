0b5cde2fbafcb58dc6706c16d9e1dda86a628048
venkata naga sai kumar kanagala <venkata.kanagala@optum.com>
2020-10-06 08:20:52 -0500

    updated for PRE Scripts

--- cukesatdd/src/main/java/atdd/runners/acquisition/ole/RunMRATDDAcquisitionOLEBlayerPRE.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/ole/RunMRATDDAcquisitionOLEBlayerPRE.java
@@ -12,13 +12,13 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionOLEBlayer
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionOLEPREPage.json",detailedReport = true,
- detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionOLEPREPage")
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionOLEBlayerPRE.json",detailedReport = true,
+ detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionOLEBlayerPRE")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },monochrome=true, features = { "src/main/resources/feature/acquisition/ole" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionOLEPREPage.json" }, tags = { "@PRE_VPP_OLE_E2E" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionOLEBlayerPRE.json" }, tags = { "@PRE_VPP_OLE_E2E_UHC" })
 
 
 
-public class RunMRATDDAcquisitionOLEPREPage {
+public class RunMRATDDAcquisitionOLEBlayerPRE {
 
 }
