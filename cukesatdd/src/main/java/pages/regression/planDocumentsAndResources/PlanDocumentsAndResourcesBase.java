49883a11b5cdd63ca5d2f0a254f921741156f670
lchoi2 <li-yan.choi@optum.com>
2020-10-06 17:01:41 -0400

    F411758:US000000-update planDoc regression script

--- cukesatdd/src/main/java/pages/regression/planDocumentsAndResources/PlanDocumentsAndResourcesBase.java
+++ cukesatdd/src/main/java/pages/regression/planDocumentsAndResources/PlanDocumentsAndResourcesBase.java
@@ -176,6 +176,11 @@ public class PlanDocumentsAndResourcesBase extends PlanDocumentsAndResourcesBase
 					section_note=validateSubPageContent(testInputInfoMap, section_note, actUrl, targetDocName);
 				}
 			} else {
+				if (!MRScenario.environment.contains("team-a")) {
+					System.out.println("Proceed to validate the PDF content");
+					String actUrl=driver.getCurrentUrl();
+					section_note=validateSubPageContent(testInputInfoMap, section_note, actUrl, targetDocName); //lyc
+				}
 				section_note.add("    SKIP - element link in href attribute vs actual link URL after clicked validation");
 			}
 			driver.close();
@@ -1127,13 +1132,13 @@ public class PlanDocumentsAndResourcesBase extends PlanDocumentsAndResourcesBase
 			}			
 		} else {
 			//note: for other section, do simpler validatoin
-			if (actUrl.contains(".pdf")) {
+			if (actUrl.contains(".pdf") || actUrl.contains("alphadog")) {
 				try {
 					URL TestURL = new URL(driver.getCurrentUrl());
 					BufferedInputStream TestFile = new BufferedInputStream(TestURL.openStream());
 					PDDocument document = PDDocument.load(TestFile);
 					String PDFText = new PDFTextStripper().getText(document);
-					//keep-for-debug System.out.println("PDF text : "+PDFText);
+					//keep-for-debug System.out.println("PDF text : "+PDFText);  
 					if(PDFText!=null && !PDFText.equals(""))
 						section_note.add("    PASSED - validated pdf content is not null or empty");
 					else {
