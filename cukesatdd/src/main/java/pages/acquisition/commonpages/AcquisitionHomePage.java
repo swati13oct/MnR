bd4d476976b814df9365f3b1e3ec28453b012432
Namita Meher <namita_meher@optum.com>
2020-10-06 23:01:33 +0530

    Updated failure for Visitor Profile

--- cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
@@ -175,7 +175,7 @@ public class AcquisitionHomePage extends GlobalWebElements {
 	@FindBy(xpath = "//*[@class='container meded-article-header']/h1']")
 	private WebElement MALandingHeading;
 
-	@FindBy(xpath = "//*[contains(@id,'zipcodebtn') or (contains(@class,'zip-button' ) and contains( text(),'Go'))]")
+	@FindBy(xpath = "//*[contains(@id,'zipcodebtn') or (@class='zip-button') and (contains(@dtmid,'plans'))]")
 	private WebElement viewPlansButton;
 	
 	@FindBy(xpath = "//form[@id='zip-form']//button[@class='zip-button']")
