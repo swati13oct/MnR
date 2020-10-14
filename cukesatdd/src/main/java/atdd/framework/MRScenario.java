fd6402eb19475ce3dde1d03e21e3a188210bec8a
Amey Mahale <amey_mahale@optum.com>
2020-10-14 10:45:04 +0530

    Updated MacOS version

--- cukesatdd/src/main/java/atdd/framework/MRScenario.java
+++ cukesatdd/src/main/java/atdd/framework/MRScenario.java
@@ -1092,7 +1092,7 @@ try {
 				sauceOptions.setCapability("idleTimeout", 200);
 
 				SafariOptions browserOptions = new SafariOptions();
-				browserOptions.setCapability("platformName", "macOS 10.14");
+				browserOptions.setCapability("platformName", "macOS 10.15");
 				browserOptions.setCapability("browserVersion", browserVersion);
 				browserOptions.setCapability("sauce:options", sauceOptions);
 				capabilities.merge(browserOptions);
