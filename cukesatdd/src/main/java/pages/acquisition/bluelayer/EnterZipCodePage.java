cada70155184cb8722edfb92083689b4b15962b1
sanjay <sanjay_takke@optum.com>
2020-10-14 18:55:13 +0530

    Commit Zip Entry Global Components

--- cukesatdd/src/main/java/pages/acquisition/bluelayer/EnterZipCodePage.java
+++ cukesatdd/src/main/java/pages/acquisition/bluelayer/EnterZipCodePage.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -38,6 +39,16 @@ public class EnterZipCodePage extends UhcDriver {
 
 	@FindBy(xpath = "(//form[contains(@class,'zipForm')]//button[contains(@class,'uhc-zip-button')])")
 	List<WebElement> ZipcodeButton;
+	
+	@FindBy(xpath = "//li[@class='expandable'][1]")
+	WebElement shopForaPlanLink;
+
+	@FindBy(xpath = "//input[@class='zip-field']")
+	WebElement shopMenuZipEntry;
+
+	@FindBy(xpath = "(//button[@class='zip-button'])[1]")
+	WebElement shopMenuZipButton;
+
 
 	public EnterZipCodePage(WebDriver driver) {
 		super(driver);
@@ -78,6 +89,29 @@ public class EnterZipCodePage extends UhcDriver {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+	public void shopMenuZipComp(String zipCode) throws Exception {
+		Actions action = new Actions(driver);
+		action.moveToElement(shopForaPlanLink).click(shopMenuZipEntry).sendKeys(zipCode).click(shopMenuZipButton).build().perform();
+
+		
+		Thread.sleep(3000);
+		String vppPageTitle = driver.getTitle();
+		if (driver.getWindowHandles().size() > 1) {
+			String currentPage = driver.getWindowHandle();
+			Set<String> newWindow = driver.getWindowHandles();
+			for (String tabs : newWindow) {
+				if (!tabs.equalsIgnoreCase(currentPage))
+					vppPageTitle = driver.switchTo().window(tabs).getTitle();
+			}
+		}
+
+		System.out.println("Actual : " + vppPageTitle);
+		System.out.println("Expected : " + PageTitleConstants.BLAYER_VPP_PLAN_PAGE_AARP_MEDICARE);
+		assertTrue("Not redirected to VPP page",
+				vppPageTitle.contains(PageTitleConstants.BLAYER_VPP_PLAN_PAGE_AARP_MEDICARE));
+		driver.navigate().back();
+
 	}
 
 	public AddDrugPage getZipCodeCounty(String zipCode, String county, String planYear) {
