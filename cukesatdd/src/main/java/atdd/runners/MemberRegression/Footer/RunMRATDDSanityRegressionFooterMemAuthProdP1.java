087b36511ce8e76c0d133c1850905f5a27fecebd
ujethwa <umang.jethwa@optum.com>
2020-10-14 16:45:55 -0500

    F360264:US000000 Updated feature file for footer for Prod ATDD

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/Footer/RunMRATDDSanityRegressionFooterMemAuthProdP1.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/Footer/RunMRATDDSanityRegressionFooterMemAuthProdP1.java
@@ -12,11 +12,11 @@ import org.junit.runner.RunWith;
  */
 
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=2,screenShotSize="", screenShotLocation="/screenshots/",
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
         jsonReport = "target/cucumber-RunMRATDDSanityRegressionFooterMemAuthProdP1.json",detailedReport = true, detailedAggregatedReport = true,
         overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDSanityRegressionFooterMemAuthProdP1")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" },
-        features = { "src/main/resources/feature/memberredesign/header" }, plugin = {
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, monochrome = true,
+        features = { "src/main/resources/feature/memberredesign/footer" }, plugin = {
         "pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDSanityRegressionFooterMemAuthProdP1.json" }, tags = { "@footer_prodSanity_IndMAPDUHC" })
 public class RunMRATDDSanityRegressionFooterMemAuthProdP1 {
 
