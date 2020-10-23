1d0872263428ef7e3dd4b3f0453d2608a5f056a4
lchoi2 <li-yan.choi@optum.com>
2020-10-23 13:43:59 -0400

    F411758:US000000-update runners for regression script

--- cukesatdd/src/main/java/atdd/runners/MemberRegression/ProviderSearch/RunMRATDDSanityProviderSearchMemAuthProdP3b_medica.java
+++ cukesatdd/src/main/java/atdd/runners/MemberRegression/ProviderSearch/RunMRATDDSanityProviderSearchMemAuthProdP3b_medica.java
@@ -9,7 +9,7 @@ import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
 
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
 jsonReport = "target/cucumber-RunMRATDDSanityProviderSearchMemAuthProdP3b_medica.json",detailedReport = true, detailedAggregatedReport = true,
 overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDSanityProviderSearchMemAuthProdP3b_medica")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.memberredesign" }, 
