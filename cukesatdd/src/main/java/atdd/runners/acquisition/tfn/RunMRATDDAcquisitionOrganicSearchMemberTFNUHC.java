02f70b487a43b9f0d4f0bd1e735708aae2dfa2d8
venkata naga sai kumar kanagala <venkata.kanagala@optum.com>
2020-11-03 07:44:52 -0600

    updated for Campaign TFN

---
+++ cukesatdd/src/main/java/atdd/runners/acquisition/tfn/RunMRATDDAcquisitionOrganicSearchMemberTFNUHC.java
@@ -0,0 +1,24 @@
+package atdd.runners.acquisition.tfn;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * this file is to run test cases for RunMRATDDAcquisitionOrganicSearchMemberTFNUHC
+ */
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDAcquisitionOrganicSearchMemberTFNUHC.json",
+detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, 
+outputFolder = "target/RunMRATDDAcquisitionOrganicSearchMemberTFNUHC")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, monochrome = true, features = { "src/main/resources/feature/acquisition/tfn" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionOrganicSearchMemberTFNUHC.json" }, tags = {"@Scenario4_7Campaign_Trafic_Member_UHC"})
+
+//@Scenario6B_GoogleSearch_AARP,@Scenario6G_YahooSearch_AARP
+public class RunMRATDDAcquisitionOrganicSearchMemberTFNUHC {
+
+}
