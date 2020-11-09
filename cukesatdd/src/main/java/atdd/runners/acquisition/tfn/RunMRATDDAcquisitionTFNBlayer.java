77c0476668e92c914f096cbceb966f090f06b930
Ramya Muddasani <r.muddasani@optum.com>
2020-11-09 14:01:03 -0600

    DevImprovement-Connectormodelplus update

--- cukesatdd/src/main/java/atdd/runners/acquisition/tfn/RunMRATDDAcquisitionTFNBlayer.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/tfn/RunMRATDDAcquisitionTFNBlayer.java
@@ -1,6 +1,5 @@
 package atdd.runners.acquisition.tfn;
 
-
 import org.junit.runner.RunWith;
 
 import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
@@ -9,14 +8,13 @@ import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
 import cucumber.api.CucumberOptions;
 
 /**
- * this file is to run test cases for TFN AARP
+ * this file is to run test cases for TFN UHC
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionTFNBlayer.json",detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionTFNBlayer")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },monochrome = true, features = { "src/main/resources/feature/acquisition/tfn" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionTFNBlayer.json" }, tags = { "@ConnectorModelPlus" })
-
-
-public class RunMRATDDAcquisitionTFNBlayer {
-
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionTFNBlayer.json",detailedReport = true,
+ detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionTFNBlayer")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/tfn" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionTFNBlayer.json" }, tags = { "@ConnectorModelPlus_UHC" })
+public class RunMRATDDAcquisitionTFNBlayer { 
+ 
 }
