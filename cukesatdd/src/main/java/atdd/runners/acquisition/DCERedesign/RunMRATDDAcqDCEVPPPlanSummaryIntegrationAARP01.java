f276329e1ac45342b08f616a3c2eb37efbdce0bb
Namita Meher <namita_meher@optum.com>
2020-11-17 13:47:57 +0530

    Updated navigation to feature files for DCE

--- cukesatdd/src/main/java/atdd/runners/acquisition/DCERedesign/RunMRATDDAcqDCEVPPPlanSummaryIntegrationAARP01.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/DCERedesign/RunMRATDDAcqDCEVPPPlanSummaryIntegrationAARP01.java
@@ -15,7 +15,7 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcqDCEVPPPlanSummaryIntegrationAARP01.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcqDCEVPPPlanSummaryIntegrationAARP01")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition" }, plugin = {
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/dceredesign" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcqDCEVPPPlanSummaryIntegrationAARP01.json" }, tags = { "@DCE_Redesign_VPP_PlanSummary_Integration_MAPD_AARP" })
 
 public class RunMRATDDAcqDCEVPPPlanSummaryIntegrationAARP01 {
