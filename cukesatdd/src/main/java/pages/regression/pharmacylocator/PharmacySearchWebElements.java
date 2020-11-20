2ebc1b4229cd5e07ccfd084f46770fd59bb135ca
ujethwa <umang.jethwa@optum.com>
2020-11-20 13:06:12 -0600

    F360264:US000000- Xpath updated for prod regression

--- cukesatdd/src/main/java/pages/regression/pharmacylocator/PharmacySearchWebElements.java
+++ cukesatdd/src/main/java/pages/regression/pharmacylocator/PharmacySearchWebElements.java
@@ -142,7 +142,7 @@ public class PharmacySearchWebElements extends UhcDriver{
 	@FindBy(xpath="//button[@title='Show street map']")
 	protected WebElement map_mapBtn;
 
-	@FindBy(xpath="//div[@role='button' and @title='Show satellite imagery']")
+	@FindBy(xpath="//button[@title='Show satellite imagery']")
 	protected WebElement map_satelliteBtn;
 
 	@FindBy(xpath="//button[@title='Toggle fullscreen view']")
