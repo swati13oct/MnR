484890326274226544a7d46af418b2411f9945eb
rmuddasa <rmuddasa@LDTU000017DJ3Z2.ms.ds.uhc.com>
2020-11-28 16:47:21 -0600

    DevImprovement-Update

--- cukesatdd/src/main/java/atdd/runners/acquisition/GlobalComponents/RunMRATDDAcquisitionHeaderAARP.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/GlobalComponents/RunMRATDDAcquisitionHeaderAARP.java
@@ -15,7 +15,7 @@ import cucumber.api.CucumberOptions;
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/globalComponents" }, plugin = {
 
 	
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionHeaderAARP.json" }, tags = { "@UATRegressionPages" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionHeaderAARP.json" }, tags = { "@globalheader_AARP" })
 
 public class RunMRATDDAcquisitionHeaderAARP {
 
