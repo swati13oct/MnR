fa4f05184374a0a115d62743c4706aba59b5b68c
Aayush <aayush.shah@optum.com>
2020-12-01 17:21:20 -0500

    Changes to Test data sheet for plan summary 2021 for formatting issues

--- cukesatdd/src/main/java/acceptancetests/acquisition/vpp/VppPlanValidationStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/acquisition/vpp/VppPlanValidationStepDefinition.java
@@ -506,7 +506,8 @@ public class VppPlanValidationStepDefinition {
 				 AepVppPlanSummaryPage planSummaryPage = null;
 				 String currentCellValue = "";
 				 String currentColName = "";
-				 int countyCellNum = 0, planYearCellNum = 0, planNameCellNum = 0;
+				// int countyCellNum = 7, planYearCellNum = 9, planNameCellNum = 6;
+				 int countyCellNum = 0, planYearCellNum =0, planNameCellNum = 0;
 				 HashMap <String, String> benefitsMap = new HashMap<String, String>();
 				 System.out.println(sheetName+ " SAUCE URL: "+ MRScenario.returnJobURL());
 				 //Looping over total rows with values
