50351867dc689c7794d7a92485855d53c0416d1e
vpalani7 <vinitha.palanisamy@optum.com>
2020-12-22 14:01:39 +0530

    feb atdd

--- cukesatdd/src/main/java/atdd/runners/acquisition/PRE/RunMRATDDAcquisitionPlanRecommendationEngineUlayerUAT.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/PRE/RunMRATDDAcquisitionPlanRecommendationEngineUlayerUAT.java
@@ -12,7 +12,7 @@ import cucumber.api.CucumberOptions;
  */
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(
-		retryCount=1, 
+		retryCount=0, 
 		screenShotSize="", 
 		screenShotLocation="/screenshots/",
 		jsonReport = "target/cucumber-RunMRATDDAcquisitionPlanRecommendationEngineUlayerUAT.json",
@@ -25,7 +25,7 @@ import cucumber.api.CucumberOptions;
 		glue = { "atdd.framework", "acceptancetests.acquisition.planRecommendationEngine" }, 
 		features = { "src/main/resources/feature/acquisition/PlanRecommendationEngine" }, 
 		plugin = {"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionPlanRecommendationEngineUlayerUAT.json" }, 
-		tags = {"@PRE_UAT_VP_PLANS_PDP"})
+		tags = {"@F545481"})
 public class RunMRATDDAcquisitionPlanRecommendationEngineUlayerUAT {
 
 }
