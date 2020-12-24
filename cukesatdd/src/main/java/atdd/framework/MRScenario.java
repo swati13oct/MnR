d7b470a3444b19e995e4ed9077820e118a419ca5
kjadha10 <kjadha10@vetsvra00008533.ms.ds.uhc.com>
2020-12-24 19:50:50 +0530

    DevImprovement: add new feature and runner for all users

--- cukesatdd/src/main/java/atdd/framework/MRScenario.java
+++ cukesatdd/src/main/java/atdd/framework/MRScenario.java
@@ -1014,8 +1014,10 @@ try {
 				DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 				capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 				//System.setProperty("webdriver.chrome.driver", pathToBinary);
-				System.setProperty("webdriver.chrome.driver", "C:\\ProgramData\\Chrome_driver_80.0.3987.16\\chromedriver.exe");
-
+				//System.setProperty("webdriver.chrome.driver", "C:\\ProgramData\\Chrome_driver_80.0.3987.16\\chromedriver.exe");
+				
+				System.setProperty("webdriver.chrome.driver", "C:\\ProgramData\\chromedriver_win32\\chromedriver.exe");
+				
 				webDriver = new ChromeDriver();
 				saveBean(CommonConstants.WEBDRIVER, webDriver);
 				return webDriver;
