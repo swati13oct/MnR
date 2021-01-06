324cd1e82f8c8dcf4b2c69a08860d528d2d790d8
pooja <pooja.minhas@optum.com>
2021-01-06 14:54:18 -0500

    Prod Deep links

--- cukesatdd/src/main/java/acceptancetests/data/MRConstants.java
+++ cukesatdd/src/main/java/acceptancetests/data/MRConstants.java
@@ -453,5 +453,11 @@ public class MRConstants {
   public static String DENTEGRA_DENTAL_DEEPLINK_URL = "https://stage-myaarpmedicare.uhc.com/dental";
   
   public static final String D_Rx_TESTHARNESS ="https://"+MRScenario.environment +"-medicare."+MRScenario.domain+"/?testharness=true";
+  public static final String PROD_UHC_DEEPLINK_URL_VirtualVisit = "https://myuhcmedicare.com/virtualvisits";
+  public static final String PROD_AARP_DEEPLINK_URL_VirtualVisit = "https://myaarpmedicare.com/virtualvisits";
+  public static final String PROD_PCP_DEEPLINK_URL_VirtualVisit = "https://mypcpmedicare.com/virtualvisits";
+  public static final String PROD_Medica_DEEPLINK_URL_VirtualVisit = "https://mymedicamedicare.com/virtualvisits";
+  public static final String PROD_SHIP_HWP ="https://myaarpmedicare.com/Extras";
+  public static final String PROD_DEEPLINK_URL_my_Documents_Edelivery ="https://medicare.uhc.com/?TARGET=SMSR/medicare/member/my-documents/overview.html%3fdeeplink=true&WT.mc_ID=8003461&mrcid=email_fed_m";
 
   }
