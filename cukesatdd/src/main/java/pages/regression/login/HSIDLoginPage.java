9588dd354a371b7e4a16650b7994ebd25602dfb3
lchoi2 <li-yan.choi@optum.com>
2021-01-12 20:25:10 -0500

    F411758:US000000-update benefits regression script

--- cukesatdd/src/main/java/pages/regression/login/HSIDLoginPage.java
+++ cukesatdd/src/main/java/pages/regression/login/HSIDLoginPage.java
@@ -767,7 +767,7 @@ public class HSIDLoginPage extends UhcDriver {
 				} catch (Exception e1) {
 					System.out.println("did not encounter 'Go To Homepage', moving on. "+e1);
 				}
-				checkModelPopup(driver, 1);
+				checkModelPopup(driver, 2);
 			} else {
 				Assert.assertTrue("PROBLEM - will only workaround the splash page on team-atest or stage env, "
 						+ "please either use another test user or manually handle the splash page properly.  "
