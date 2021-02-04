81d7ea99260ee7b25671384a2c75c577ac5b54c0
venkata naga sai kumar kanagala <venkata.kanagala@optum.com>
2021-02-04 08:36:16 -0600

    updated

--- cukesatdd/src/main/java/atdd/runners/acquisition/campaignExternalLinkE2E/RunMRATDDAcquisitionOLEUlayerCampaignExternalLink.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/campaignExternalLinkE2E/RunMRATDDAcquisitionOLEUlayerCampaignExternalLink.java
@@ -12,9 +12,9 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionOLEBlayer
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionOLEUlayerCampaignExternalLink.json",detailedReport = true,
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionOLEUlayerCampaignExternalLink.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionOLEUlayerCampaignExternalLink")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },monochrome=true, features = { "src/main/resources/feature/acquisition/ole" }, plugin = {
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },monochrome=true, features = { "src/main/resources/feature/acquisition/CampaignExternalLinksE2E" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionOLEUlayerCampaignExternalLink.json" }, tags = { "@CampaignExternalLink_E2E_Scenario_6" })
 
 
