51163634af3ace6ddc12817009576e0cf802414c
Rathulya <rathulya_ravindran@optum.com>
2021-02-10 11:52:07 +0530

    Fix in xpath footer

--- cukesatdd/src/main/java/pages/acquisition/ulayer/AboutUsAARPPage.java
+++ cukesatdd/src/main/java/pages/acquisition/ulayer/AboutUsAARPPage.java
@@ -17,7 +17,7 @@ import acceptancetests.util.CommonUtility;
 
 public class AboutUsAARPPage extends GlobalWebElements{
 
-	@FindBy(xpath = "//*[contains(@class,'meded-article-header__title')]")
+	@FindBy(xpath = "//*[contains(@class,'heading-1')]")
 	public static WebElement header;
 	
 	@FindBy(xpath = "//section[contains(@class,'meded-article-content')]//p[string-length(text()) > '1']")
