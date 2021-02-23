4e8152e27519372eaa4e6739c1d5c3be1884cfa1
Sneha Dwarakanath <sneha.dwarakanath@optum.com>
2021-02-23 18:21:23 -0500

    Added waitforpageload, for search and add drug step. Check page ready is not working for Pharmacy to dce flow.

--- cukesatdd/src/main/java/atdd/runners/acquisition/DCERedesign/RunMRATDDAcqDCEUATscenario1UHC.java
+++ cukesatdd/src/main/java/atdd/runners/acquisition/DCERedesign/RunMRATDDAcqDCEUATscenario1UHC.java
@@ -13,7 +13,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcqDCEUATscenario1UHC - Scenario to validate UAT end-2-end scenario1 for DCE on UHC site
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcqDCEUATscenario1UHC.json",detailedReport = true,
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcqDCEUATscenario1UHC.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcqDCEUATscenario1UHC")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/dceredesign" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcqDCEUATscenario1UHC.json" }, tags = { "@DCE_E2E_Scenario1_UAT_UHC" })
