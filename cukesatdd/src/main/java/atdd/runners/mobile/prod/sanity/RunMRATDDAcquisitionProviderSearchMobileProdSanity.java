7af56e183f2db139515a7188379133347bf64e59
hahire <harshal_ahire@optum.com>
2021-03-03 23:26:48 +0530

    Runnerchanges

--- cukesatdd/src/main/java/atdd/runners/mobile/prod/sanity/RunMRATDDAcquisitionProviderSearchMobileProdSanity.java
+++ cukesatdd/src/main/java/atdd/runners/mobile/prod/sanity/RunMRATDDAcquisitionProviderSearchMobileProdSanity.java
@@ -11,7 +11,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionProviderSearchMobileProdSanity
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",
 jsonReport = "target/cucumber-RunMRATDDAcquisitionProviderSearchMobileProdSanity.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionProviderSearchMobileProdSanity")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.mobile" }, features = { "src/main/resources/feature/mobile/acquisition/providersearch" }, 
--- cukesatdd/src/main/java/atdd/runners/mobile/prod/sanity/ios/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3ProdSanity.java
+++ cukesatdd/src/main/java/atdd/runners/mobile/prod/sanity/ios/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3ProdSanity.java
@@ -11,7 +11,7 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3.json",
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3.json",
 detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.mobile.acquisition" }, features = { "src/main/resources/feature/mobile/acquisition/pharmacy/PharmacySearch-mobile-spanish.feature" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP3.json" }, tags = { "@ios"})
