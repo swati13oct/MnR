933709502f0b35ce9911784504098eab101b5107
Namita Meher <namita_meher@optum.com>
2021-03-04 09:07:33 +0530

    Updated for Prod job

--- cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdPharmacyLocatorUlayer.java
+++ cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdPharmacyLocatorUlayer.java
@@ -15,7 +15,7 @@ import cucumber.api.CucumberOptions;
 detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionProdPharmacyLocatorUlayer")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = {
 		"src/main/resources/feature/acquisition/pharmacylocator/PharmacySearch-Common.feature" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionProdPharmacyLocatorUlayer.json" }, tags = { "@prodRegression" })
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionProdPharmacyLocatorUlayer.json" }, tags = { "@prodRegression_AARP" })
 public class RunMRATDDAcquisitionProdPharmacyLocatorUlayer{ 
  
 }
