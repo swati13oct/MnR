558581fc09f8e712b2317e2ef0dd0fc8b4c96f1d
hahire <harshal_ahire@optum.com>
2021-03-15 22:44:40 +0530

    Updated Prod sanity runners

--- cukesatdd/src/main/java/atdd/runners/mobile/prod/sanity/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7ProdSanity.java
+++ cukesatdd/src/main/java/atdd/runners/mobile/prod/sanity/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7ProdSanity.java
@@ -11,10 +11,10 @@ import cucumber.api.CucumberOptions;
  * this file is to run test cases for RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP2
  */
 @RunWith(ExtendedCucumber.class)
-@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7.json",
-detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7")
+@ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7ProdSanity.json",
+detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7ProdSanity")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.mobile.acquisition" }, features = { "src/main/resources/feature/mobile/acquisition/pharmacy/PharmacySearch-mobile.feature" }, plugin = {
-		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7.json" }, tags = { "@pharmacylocatorblayer02","@pharmacylocatorblayermobile"})
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7ProdSanity.json" }, tags = { "@pharmacylocatorblayer02","@pharmacylocatorblayermobile"})
 public class RunMRATDDAcquisitionPharmacyLocatorMobileUlayerP7ProdSanity{ 
  
 }
