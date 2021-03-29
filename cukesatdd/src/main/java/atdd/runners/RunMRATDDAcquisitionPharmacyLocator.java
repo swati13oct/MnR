4a92ab968b96ea93dfce44cd2152d06e6bf29e47
Amey Mahale <amey_mahale@optum.com>
2021-03-29 19:19:14 +0530

    Global runner for microapp

---
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPharmacyLocator.java
@@ -0,0 +1,24 @@
+package atdd.runners;
+
+import org.testng.annotations.Test;
+
+import atdd.framework.BaseTestConfig;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },
+				features = {"src/main/resources/feature/acquisition/pharmacylocator"},
+				monochrome = true,
+				plugin = { "pretty",
+						"html:reports/test-report.html",
+						"json:target/cucumber-RunMRATDDAcquisitionPharmacyLocator.json",
+						"timeline:target" },
+				tags = "@regression")
+
+public class RunMRATDDAcquisitionPharmacyLocator extends BaseTestConfig {
+	@Test(dataProvider = ScenarioDataProvider)
+	public void runCukes(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+		testNGCucumberRunner.runScenario(pickleWrapper.getPickle());
+	}
+}
