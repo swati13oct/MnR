741ad23fe168665b1a9cf3f4ca596d44ac0a1352
Namita Meher <namita_meher@optum.com>
2021-03-31 19:36:31 +0530

    Updated runners

--- cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdProviderSearchUlayer.java
+++ cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdProviderSearchUlayer.java
@@ -13,7 +13,8 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionProdProviderSearchUlayer.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionProdProviderSearchUlayer")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/providersearch/" }, plugin = {
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = {
+		"src/main/resources/feature/acquisition/providersearch/ProviderSearch-UAT.feature" }, plugin = {
 		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionProdProviderSearchUlayer.json" }, tags = { "@ProviderSearchCommon_Prod_AARP"})//"@prodRegression" })
 public class RunMRATDDAcquisitionProdProviderSearchUlayer{ 
  
--- cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdVisitorProfileUlayer04.java
+++ cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdVisitorProfileUlayer04.java
@@ -13,7 +13,8 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount = 0, screenShotSize = "", screenShotLocation = "/screenshots/", jsonReport = "target/cucumber-RunMRATDDAcquisitionProdVisitorProfileUlayer04.json", detailedReport = true, detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionProdVisitorProfileUlayer04")
 @CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = {
-		"src/main/resources/feature/acquisition/visitorProfile/" }, plugin = { "pretty", "html:reports/test-report",
+		"src/main/resources/feature/acquisition/visitorProfile/visitorProfile_UnAuthenticated.feature" }, plugin = {
+				"pretty", "html:reports/test-report",
 				"json:target/cucumber-RunMRATDDAcquisitionProdVisitorProfileUlayer04.json" }, tags = {
 						"@prodRegression_AARP_04" })
 public class RunMRATDDAcquisitionProdVisitorProfileUlayer04 {
