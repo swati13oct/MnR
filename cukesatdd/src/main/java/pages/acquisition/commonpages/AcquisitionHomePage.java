6fe670e9f06ece0cb1fb1ec04cefe7cab15dd591
Rathulya <rathulya_ravindran@optum.com>
2021-04-01 20:45:34 +0530

    Fixes in script

--- cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
@@ -218,7 +218,7 @@ public class AcquisitionHomePage extends GlobalWebElements {
 
 	//@FindBy(xpath = "//*[contains(@id,'zipcodebtn') or (@class='zip-button') and (contains(@dtmid,'plans'))]")
 	//@FindBy(xpath = "//*[contains(@class,'uhc-zip-button')]")
-	@FindBy(xpath = "//*[contains(@class,'uhc-zip-button') or contains(@id,'zipcodebtn')]")
+	@FindBy(xpath = "(//*[contains(@class,'zip-button') or contains(@id,'zipcodebtn')])[2]")
 	private WebElement viewPlansButton;
 
 	@FindBy(xpath = "//form[@id='zip-form']//button[@class='zip-button']")
