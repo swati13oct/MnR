3c8ad5c20ca9a7f8b80ed013c24db57a7bb501ea
Pkaur14 <Pawandeep.kaur@optum.com>
2021-04-05 17:00:52 -0500

    Added New Scenario

---
+++ cukesatdd/src/main/java/atdd/runners/acquisition/GlobalComponents/RunMRATDDAcquisitionHeaderFooterGeoTargetLink.java
@@ -0,0 +1,22 @@
+package atdd.runners.acquisition.GlobalComponents;
+
+import org.junit.runner.RunWith;
+
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumber;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+
+import cucumber.api.CucumberOptions;
+
+/**
+ * this file is to run test cases for RunMRATDDAcquisitionHeaderUHC
+ */
+@RunWith(ExtendedCucumber.class)
+@ExtendedCucumberOptions(retryCount=1,screenShotSize="", screenShotLocation="/screenshots/",
+jsonReport = "target/cucumber-RunMRATDDAcquisitionHeaderFooterGeoTargetLink.json",detailedReport = true,
+detailedAggregatedReport = true, overviewReport = true, toPDF = true, 
+outputFolder = "target/RunMRATDDAcquisitionHeaderFooterGeoTargetLink")
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, features = { "src/main/resources/feature/acquisition/globalComponents" }, plugin = {
+		"pretty", "html:reports/test-report", "json:target/cucumber-RunMRATDDAcquisitionHeaderFooterGeoTargetLink.json" }, tags = { " @GeoTarget_MedSup_GlobalCompsAARP" })
+public class RunMRATDDAcquisitionHeaderFooterGeoTargetLink {
+
+}
