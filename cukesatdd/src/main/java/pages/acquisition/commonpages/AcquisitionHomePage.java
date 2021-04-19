3c9e0063531f6e1bf87d0f5cc328af2528a220ea
Rathulya <rathulya_ravindran@optum.com>
2021-04-19 20:58:46 +0530

    Fixes in xpath

--- cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
@@ -222,7 +222,7 @@ public class AcquisitionHomePage extends GlobalWebElements {
 //	@FindBy(xpath = "(//*[contains(@class,'zip-button') or contains(@id,'zipcodebtn')])[2]")
 	//@FindBy(xpath = "(//button[contains(@class,'zip-button') or contains(@id,'zipcodebtn')])[2]")
 	//@FindBy(xpath = "//*[contains(@class,'zip-button') or contains(@id,'zipcodebtn')]"
-	@FindBy(xpath = "(//button[contains(@class,'zip-button') or contains(@id,'zipcodebtn')])[2]")
+	@FindBy(xpath = "//*[contains(@class,'zip-button') or contains(@id,'zipcodebtn')]")
 	private WebElement viewPlansButton;
 
 	@FindBy(xpath = "//form[@id='zip-form']//button[@class='zip-button']")
