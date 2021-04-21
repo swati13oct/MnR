769c02421229a8d570538761c28033fc529b7e9f
Namita Meher <namita_meher@optum.com>
2021-04-21 11:57:16 +0530

    Updated feature path for OLE PDP Sanity runner

--- cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdOLEBlayerPDPplan.java
+++ cukesatdd/src/main/java/atdd/runners/Prod/RunMRATDDAcquisitionProdOLEBlayerPDPplan.java
@@ -14,7 +14,8 @@ import cucumber.api.CucumberOptions;
 @RunWith(ExtendedCucumber.class)
 @ExtendedCucumberOptions(retryCount=0,screenShotSize="", screenShotLocation="/screenshots/",jsonReport = "target/cucumber-RunMRATDDAcquisitionProdOLEBlayerPDPplan.json",detailedReport = true,
  detailedAggregatedReport = true, overviewReport = true, toPDF = true, outputFolder = "target/RunMRATDDAcquisitionProdOLEBlayerPDPplan")
-@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" },monochrome=true, features = { "src/main/resources/feature/acquisition/ole/oleFuture/OLE-PlanSummary-PDP.feature" }, plugin = {
+@CucumberOptions(glue = { "atdd.framework", "acceptancetests.acquisition" }, monochrome = true, features = {
+		"src/main/resources/feature/acquisition/ole/oleFuture/OLE-PlanDetails-PDP.feature" }, plugin = {
 				"pretty", "html:reports/test-report",
 				"json:target/cucumber-RunMRATDDAcquisitionProdOLEBlayerPDPplan.json" }, tags = {
 						"@prodRegression_UHC" })
