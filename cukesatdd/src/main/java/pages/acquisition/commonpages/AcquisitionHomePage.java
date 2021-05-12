a6ba669ad40d65c3eaaaf69b718024dd2854a287
Rathulya <rathulya_ravindran@optum.com>
2021-05-12 14:24:30 +0530

    Fixes in script

--- cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
@@ -6368,7 +6368,7 @@ public class AcquisitionHomePage extends GlobalWebElements {
 			Assertion.assertTrue("Navigation to Medicare Advantage Plans page failed", driver.getCurrentUrl().contains("/medicare-education/medicare-advantage-plans.html"));
 			break;
 			
-		case "Medicare Supplement Insurance Plans":
+		case "Medicare Supplement Insurance":
 			Assertion.assertTrue("Navigation to Medicare Supplement Insurance Plans page failed", driver.getCurrentUrl().contains("/medicare-education/medicare-supplement-plans.html"));
 			break;
 			
@@ -6376,8 +6376,8 @@ public class AcquisitionHomePage extends GlobalWebElements {
 			Assertion.assertTrue("Navigation to Medicare Prescription Drug Plans page failed", driver.getCurrentUrl().contains("/medicare-education/medicare-part-d.html"));
 			break;
 			
-		case "Enrollment Basics":
-			Assertion.assertTrue("Navigation to Enrollment Basics page failed", driver.getCurrentUrl().contains("/medicare-education/enrollment-and-changing-plans.html"));
+		case "When to Enroll":
+			Assertion.assertTrue("Navigation to Enrollment Basics page failed", driver.getCurrentUrl().contains("/medicare-education/when-to-enroll.html"));
 			break;
 			
 		case "Medicare FAQ":
