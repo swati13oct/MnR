29f17d90109f66841377aec50f1c21bc2aeeeede
Amey Mahale <amey_mahale@optum.com>
2021-05-18 16:31:21 +0530

    Reverting incorrect changes

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionGlobalComponents.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionGlobalComponents.java
@@ -15,9 +15,9 @@ import io.cucumber.testng.PickleWrapper;
 						"html:reports/test-report.html",
 						"json:target/cucumber-RunMRATDDAcquisitionGlobalComponents.json",
 						"timeline:target" },
-				tags = "@amey")
+				tags = "@regressionAARP")
 
-@RetryCountIfFailed(0)
+@RetryCountIfFailed(1)
 public class RunMRATDDAcquisitionGlobalComponents extends BaseTestConfig{
 	@Test(dataProvider = ScenarioDataProvider)
 	public void runCukes(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
