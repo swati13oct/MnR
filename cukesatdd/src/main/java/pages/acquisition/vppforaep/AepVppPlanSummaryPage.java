eec0d3968a81c9ae1cffdae0cdc8543999cdad43
greddy23 <greddy23@LHTU05CG0391ZVF.ms.ds.uhc.com>
2021-05-23 10:58:24 +0530

    Added plan benfit validation pages for plan compare and details page.

--- cukesatdd/src/main/java/pages/acquisition/vppforaep/AepVppPlanSummaryPage.java
+++ cukesatdd/src/main/java/pages/acquisition/vppforaep/AepVppPlanSummaryPage.java
@@ -3,19 +3,26 @@
  */
 package pages.acquisition.vppforaep;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
+import org.json.JSONObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import pages.acquisition.ole.WelcomePage;
+import acceptancetests.data.ElementData;
 import acceptancetests.util.CommonUtility;
 import atdd.framework.UhcDriver;
-import pages.acquisition.vpp.AepPlanDetailsPage;
 
 /**
  * @author 
