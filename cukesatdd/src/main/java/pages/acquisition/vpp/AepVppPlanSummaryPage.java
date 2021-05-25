26232114e96b22a22e70d5fb6173cf402279ede8
venkata naga sai kumar kanagala <venkata.kanagala@optum.com>
2021-05-25 14:10:50 -0500

    updated for vpp

--- cukesatdd/src/main/java/pages/acquisition/vpp/AepVppPlanSummaryPage.java
+++ cukesatdd/src/main/java/pages/acquisition/vpp/AepVppPlanSummaryPage.java
@@ -707,7 +707,7 @@ public class AepVppPlanSummaryPage extends UhcDriver {
 
 				WebElement enrollInPlan = null;
 					try {
-						Thread.sleep(5000);
+						//Thread.sleep(5000);
 				if(planType.equalsIgnoreCase("MA")) {
 
 					validate(driver.findElement(By.xpath("//*[contains(text(),'Enroll in plan')]")));
@@ -727,6 +727,7 @@ public class AepVppPlanSummaryPage extends UhcDriver {
 				if (enrollInPlan != null) {
 					validateNew(enrollInPlan);
 					jsClickNew(enrollInPlan);
+					validateNew(welcomePageHeader,60);
 				}
 		return flag;
 		}
