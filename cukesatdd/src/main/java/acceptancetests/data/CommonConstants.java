7492e8a40be00f755d436d4d1107354813dc8530
Amey Mahale <amey_mahale@optum.com>
2021-06-14 16:05:29 +0530

    [OLE MA] Removed final keyword for GPS_QUERY

--- cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
+++ cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
@@ -890,7 +890,9 @@ public class CommonConstants {
 	//public static final String CONNECTION_URL = "jdbc:oracle:thin:qaread/testreadonly@orass0464:1521/gpsst04svc_trn.uhc.com";
 	
 	public static final String CONNECTION_URL = "jdbc:oracle:thin:qaread/testreadonly@dbslt0102:1521/gpsts18";
-	public static final String GPS_QUERY = "Select MEDICAID_IND,\n" + 
+	
+	//GPS Query cannot be final variable, since a stage id is to be appended at runtime
+	public static String GPS_QUERY = "Select MEDICAID_IND,\n" + 
 			"MEDICAID_NUMBER,\n" + 
 			"MAILING_ZIP_CD,\n" + 
 			"MAILING_STATE_CD,\n" + 
