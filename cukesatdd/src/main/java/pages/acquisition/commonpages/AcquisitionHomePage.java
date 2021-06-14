f07774a2feeb543cc799c24d57ec23082b5eccf4
Sneha Dwarakanath <sneha.dwarakanath@optum.com>
2021-06-14 16:44:16 -0400

    DCE link on Med Ed page opening in new tab
    Signed-off-by: Sneha Dwarakanath <sneha.dwarakanath@optum.com>

--- cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/commonpages/AcquisitionHomePage.java
@@ -3324,17 +3324,13 @@ public class AcquisitionHomePage extends GlobalWebElements {
 		// contains(@title, 'prescription drug costs')]"));
 		WebElement DCELink = driver.findElement(By.xpath(
 				"//a[contains(@href,'drug-cost-estimator') and (contains(@title, 'prescription drug costs') or @onkeydown)]"));
-
+		validateNew(DCELink);
 		String winHandleBefore = driver.getWindowHandle();
-/*
 		switchToNewTabNew(DCELink);
 		String winHandleCurrent = driver.getWindowHandle();
 		driver.switchTo().window(winHandleBefore);
 		driver.close();
 		driver.switchTo().window(winHandleCurrent);
-*/
-	validateNew(DCELink);
-	jsClickNew(DCELink);
 		CommonUtility.waitForPageLoadNew(driver, AddMyDrugsBtn, 20);
 		if (driver.getCurrentUrl().contains("drug-cost-estimator"))
 			return new GetStartedPage(driver);
