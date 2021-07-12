6d6c228f912a0a4d2dfef288f6fba5d6bb7194ae
Murugasamy <murali.murugasamy@optum.com>
2021-07-12 17:38:46 +0530

    Runner

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPlanRecommendationEngine.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPlanRecommendationEngine.java
@@ -16,7 +16,7 @@ import io.cucumber.testng.PickleWrapper;
 						"html:reports/test-report.html",
 						"json:target/cucumber-RunMRATDDAcquisitionPlanRecommendationEngine.json",
 						"timeline:target" },
-				tags = "@PDPFlowRanking1234")
+				tags = "@PREPlanBenefitsAARP")
 
 @RetryCountIfFailed(0)
 public class RunMRATDDAcquisitionPlanRecommendationEngine extends BaseTestConfig {
