c320fd5b3a4c236542f1d9bedf587198c589090d
Rathulya <rathulya_ravindran@optum.com>
2021-07-19 16:41:51 +0530

    Fixes in script

--- cukesatdd/src/main/java/pages/acquisition/commonpages/PrivacyPolicyAARPPage.java
+++ cukesatdd/src/main/java/pages/acquisition/commonpages/PrivacyPolicyAARPPage.java
@@ -18,7 +18,7 @@ public class PrivacyPolicyAARPPage extends GlobalWebElements{
  
 	//@FindBy(xpath = "//*[contains(@class,'meded-article-header__title')]")
 	//@FindBy(xpath="//span[contains(@class,'heading-1')]")
-	@FindBy(xpath="(//span[contains(@class,'heading-1')])[3]")
+	@FindBy(xpath="(//span[contains(@class,'heading-1')])[2]")
 	public WebElement header;
 	
 	//@FindBy(xpath = "(//section[contains(@class,'meded-article-content')]//p)[1]")
