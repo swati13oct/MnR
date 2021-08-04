c184f054fb2b6e2567a7f99a3186b0840336c1b0
Amey Mahale <amey_mahale@optum.com>
2021-08-04 14:16:33 +0530

    [DCE] Fixes for DCE-UAT scenario

--- cukesatdd/src/main/java/acceptancetests/mobile/acquisition/dceredesign/DCEACQNewRunnerMobile.java
+++ cukesatdd/src/main/java/acceptancetests/mobile/acquisition/dceredesign/DCEACQNewRunnerMobile.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import acceptancetests.acquisition.dceredesign.DCERedesignCommonConstants;
@@ -281,10 +282,7 @@ public class DCEACQNewRunnerMobile {
 				.getBean(PageConstants.DCE_Redesign_TellUsAboutDrug);
 		BuildYourDrugListMobile DCEbuildDrugList = tellUsAboutDrug.ClickAddDrug();
 		String druglist = (String) getLoginScenario().getBean(DCERedesignCommonConstants.DRUGLIST);
-		if (null == druglist) {
-			druglist = "";
-		}
-		druglist = druglist + "&" + drugName;
+		druglist = StringUtils.isEmpty(druglist) ? drugName : druglist + "&" + drugName;
 		System.out.println("Drugs List : " + druglist);
 		getLoginScenario().saveBean(DCERedesignCommonConstants.DRUGLIST, druglist);
 	}
