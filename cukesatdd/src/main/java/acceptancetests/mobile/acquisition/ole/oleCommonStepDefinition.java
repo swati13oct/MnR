bdc7fc936b1eb8bacd7d97f529c1e68ca9a7e5ec
Amey Mahale <amey_mahale@optum.com>
2021-08-05 18:52:53 +0530

    [VPP-OLE] Adding missing step definition

--- cukesatdd/src/main/java/acceptancetests/mobile/acquisition/ole/oleCommonStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/mobile/acquisition/ole/oleCommonStepDefinition.java
@@ -139,4 +139,26 @@ public class oleCommonStepDefinition {
 			Assertion.fail("Error in validating the OLE Welcome Page");
 
 	}
+	
+	@Then("^The User validates the Plan details on OLE page$")
+	public void the_user_validates_the_Plan_details_on_OLE() throws Throwable {
+
+		WelcomePageMobile welcomePage = (WelcomePageMobile) getLoginScenario().getBean(OLE_PageConstants.OLE_WELCOME_PAGE);
+		Map<String, String> PlanDetailsMap = new HashMap<String, String>();
+		PlanDetailsMap.put("Plan Name", (String) getLoginScenario().getBean(oleCommonConstants.OLE_PLAN_NAME));
+		//PlanDetailsMap.put("Plan Year", (String) getLoginScenario().getBean(oleCommonConstants.OLE_PLAN_YEAR));
+		PlanDetailsMap.put("Zip Code", (String) getLoginScenario().getBean(oleCommonConstants.OLE_ZIPCODE));
+		PlanDetailsMap.put("County", (String) getLoginScenario().getBean(oleCommonConstants.OLE_COUNTY));
+		PlanDetailsMap.put("Plan Premium", (String) getLoginScenario().getBean(oleCommonConstants.OLE_PLAN_PREMIUM));
+
+		boolean Validation_Status = welcomePage.validate_plan_details(PlanDetailsMap);
+		if (Validation_Status) {
+			System.out.println("Plan Details Validation in OLE PAGE : " + Validation_Status + " - Validation Passed");
+			getLoginScenario().saveBean(OLE_PageConstants.OLE_WELCOME_PAGE, welcomePage);
+			Assertion.assertTrue(true);
+		} else {
+			System.out.println("Plan Details Validation in OLE PAGE : " + Validation_Status);
+			Assertion.fail();
+		}
+	}
 }
