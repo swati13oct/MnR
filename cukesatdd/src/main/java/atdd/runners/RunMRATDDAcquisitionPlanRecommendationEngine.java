fac72f02f69d1062695ba643364e9dd21f7de9de
Murugasamy <murali.murugasamy@optum.com>
2021-08-10 16:39:16 +0530

    New MS update

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPlanRecommendationEngine.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPlanRecommendationEngine.java
@@ -16,7 +16,7 @@ import io.cucumber.testng.PickleWrapper;
 						"html:reports/test-report.html",
 						"json:target/cucumber-RunMRATDDAcquisitionPlanRecommendationEngine.json",
 						"timeline:target" },
-				tags = "@MS_PREBENEFITS_Temp_AARP")
+				tags = "@PDP_PREBENEFITS_Temp_AARP")
 
 @RetryCountIfFailed(0)
 public class RunMRATDDAcquisitionPlanRecommendationEngine extends BaseTestConfig {
