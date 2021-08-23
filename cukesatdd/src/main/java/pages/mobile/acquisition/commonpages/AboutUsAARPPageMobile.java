1c6e19de45b2ebbbba78ab9acd807ad8fcedb728
Amey Mahale <amey_mahale@optum.com>
2021-08-23 12:47:41 +0530

    [HeaderFooter] Updated locator for about us page header

--- cukesatdd/src/main/java/pages/mobile/acquisition/commonpages/AboutUsAARPPageMobile.java
+++ cukesatdd/src/main/java/pages/mobile/acquisition/commonpages/AboutUsAARPPageMobile.java
@@ -17,7 +17,7 @@ import acceptancetests.util.CommonUtility;
 
 public class AboutUsAARPPageMobile extends GlobalWebElements{
 
-	@FindBy(xpath = "//div[contains(@class,'breadcrumb aem-GridColumn aem-GridColumn--default--12')]")
+	@FindBy(xpath = "//h1[contains(text(),'About us')]")
 	public WebElement header;
 	
 	@FindBy(xpath = "//span[contains(text(),'UnitedHealthcare is dedicated to helping people li')]")
