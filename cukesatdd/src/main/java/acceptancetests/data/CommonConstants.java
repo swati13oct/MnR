8bf68df5d36d24e990477b12d1ed1346254a35ce
Amey Mahale <amey_mahale@optum.com>
2021-09-09 16:39:19 +0530

    [OLE] Fixes for ole-uat scenarios

--- cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
+++ cukesatdd/src/main/java/acceptancetests/data/CommonConstants.java
@@ -1038,6 +1038,8 @@ public class CommonConstants {
 	public static final String FED_TFN="fedTfn";
 
 	public static final String MEDSUP_TFN="medsupTfn";
+
+	public static final String TFN="tfn";
 	
 	public static final String FOOTER_LINK = "linkName";
 	
