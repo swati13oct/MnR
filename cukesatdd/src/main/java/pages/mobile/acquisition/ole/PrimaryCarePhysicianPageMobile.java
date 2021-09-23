ae9d71d871f157d1a2fe85210bd2b15c7472b34f
Amey Mahale <amey_mahale@optum.com>
2021-09-23 15:09:49 +0530

    [OLE] Fixing locator for offline (partial content for 9/25)

--- cukesatdd/src/main/java/pages/mobile/acquisition/ole/PrimaryCarePhysicianPageMobile.java
+++ cukesatdd/src/main/java/pages/mobile/acquisition/ole/PrimaryCarePhysicianPageMobile.java
@@ -398,7 +398,8 @@ public class PrimaryCarePhysicianPageMobile extends UhcDriver {
 		flag = radioBtn.getAttribute("class").equalsIgnoreCase("ng-star-inserted active");
 		//Assertion.assertTrue("PCP is not highlighted by blue colour", flag);
 		String actualProvider = driver.findElement(By.xpath("(//*[@class='inputradio'])[1]//following-sibling::label/span")).getText();
-		String expectedProvider= driver.findElement(By.xpath("//p[text()='Doctor/Provider or PCP Full Name: ']//following-sibling::p[contains(@class,'provider-info__data')][1]")).getText().trim();
+//		String expectedProvider= driver.findElement(By.xpath("//p[text()='Doctor/Provider or PCP Full Name: ']//following-sibling::p[contains(@class,'provider-info__data')][1]")).getText().trim();
+		String expectedProvider= driver.findElement(By.xpath("//p[contains(text(),'Provider or PCP Full Name: ')]//following-sibling::p[contains(@class,'provider-info__data')][1]")).getText().trim();
 		String PCPNumber= driver.findElement(By.xpath("//p[text()='Doctor/Provider/PCP Number: ']//following-sibling::p[contains(@class,'provider-info__data')]")).getText();
 		
 		System.out.println("PCP Name is Displayed"+actualProvider);
