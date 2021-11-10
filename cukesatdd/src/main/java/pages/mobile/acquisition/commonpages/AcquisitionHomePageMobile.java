024d087c182f60439062d9fd3911237aaaa66e13
Amey Mahale <amey_mahale@optum.com>
2021-11-10 19:47:14 +0530

    Fixes for header footer

--- cukesatdd/src/main/java/pages/mobile/acquisition/commonpages/AcquisitionHomePageMobile.java
+++ cukesatdd/src/main/java/pages/mobile/acquisition/commonpages/AcquisitionHomePageMobile.java
@@ -4967,6 +4967,10 @@ public class AcquisitionHomePageMobile extends GlobalWebElements {
 					}
 				}
 				driver.switchTo().window(base);
+			} else {
+				//Step to click browser back is commented in feature file, not sure of its impact on desktop script if un commented.
+				// hence adding navigate back if the member site loads in same window. Remove this else block if the browser back step is added.
+				driver.navigate().back();
 			}
 		}
 	}
