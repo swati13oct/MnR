6559b5dc1774d728cbf7d4ad6232d70aca4fa43b
Murugasamy <murali.murugasamy@optum.com>
2021-12-17 16:54:14 +0530

    new MS states

--- cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPlanRecommendationEngine.java
+++ cukesatdd/src/main/java/atdd/runners/RunMRATDDAcquisitionPlanRecommendationEngine.java
@@ -18,7 +18,7 @@ import io.cucumber.testng.PickleWrapper;
 						"timeline:target" },
 				tags = "@regressionAARP")
 
-@RetryCountIfFailed(2)
+@RetryCountIfFailed(1)
 public class RunMRATDDAcquisitionPlanRecommendationEngine extends BaseTestConfig {
 	@Test(dataProvider = ScenarioDataProvider)
 	public void runCukes(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
