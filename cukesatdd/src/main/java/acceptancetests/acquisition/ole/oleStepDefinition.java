7751ddb802eb8fe3083e448b9cd731d8bdaae8d3
vkanagal <venkata.kanagala@optum.com>
2022-04-04 20:08:59 -0500

    updated for Prescription drug feature

--- cukesatdd/src/main/java/acceptancetests/acquisition/ole/oleStepDefinition.java
+++ cukesatdd/src/main/java/acceptancetests/acquisition/ole/oleStepDefinition.java
@@ -2472,12 +2472,13 @@ public class oleStepDefinition {
 			throws Throwable {
 
 		scenario.log("Sai - Change made 07/27- Validate to Predscription drug Coverage page   --Aug Release");
-		
+
 		Map<String, String> MemberDetailsMap = new HashMap<String, String>();
 		MemberDetailsMap = DataTableParser.readDataTableAsMaps(arg1);
-		
+		String planType = (String) getLoginScenario().getBean(oleCommonConstants.OLE_PLAN_TYPE);
+
 		MedicareInformationPage medicareInfoPage = (MedicareInformationPage) getLoginScenario().getBean(OLE_PageConstants.OLE_MEDICARE_INFO_PAGE);
-		boolean medicareInfoPageLongTerm = medicareInfoPage.answer_following_questions_PrescriptionCoverage(MemberDetailsMap);
+		boolean medicareInfoPageLongTerm = medicareInfoPage.answer_following_questions_PrescriptionCoverage(MemberDetailsMap,planType);
 		if (medicareInfoPageLongTerm) {
 			getLoginScenario().saveBean(OLE_PageConstants.OLE_MEDICARE_INFO_PAGE,
 					medicareInfoPage);
@@ -3461,9 +3462,10 @@ public class oleStepDefinition {
 
 		Map<String, String> MemberDetailsMap = new HashMap<String, String>();
 		MemberDetailsMap = DataTableParser.readDataTableAsMaps(arg1);
-		
+		String planType = (String) getLoginScenario().getBean(oleCommonConstants.OLE_PLAN_TYPE);
+
 		MedicareInformationPage medicareInfoPage = (MedicareInformationPage) getLoginScenario().getBean(OLE_PageConstants.OLE_MEDICARE_INFO_PAGE);
-		boolean medicareInfoPageLongTerm = medicareInfoPage.answer_following_questions_PrescriptionCoverage_PDP_Plans(MemberDetailsMap);
+		boolean medicareInfoPageLongTerm = medicareInfoPage.answer_following_questions_PrescriptionCoverage_PDP_Plans(MemberDetailsMap,planType);
 		if (medicareInfoPageLongTerm) {
 			getLoginScenario().saveBean(OLE_PageConstants.OLE_MEDICARE_INFO_PAGE,
 					medicareInfoPage);
