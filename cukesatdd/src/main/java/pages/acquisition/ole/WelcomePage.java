7029d70ce551a49b712ac2d76b5811f794574434
vkanagal <venkata.kanagala@optum.com>
2022-04-12 17:48:28 -0500

    Updated FOR Welcome OLE Page Plan details button

--- cukesatdd/src/main/java/pages/acquisition/ole/WelcomePage.java
+++ cukesatdd/src/main/java/pages/acquisition/ole/WelcomePage.java
@@ -251,7 +251,7 @@ public class WelcomePage extends UhcDriver{
 				if (!Expected_PlanType.equalsIgnoreCase("SNP")) {
 					String elementPath = "(//*[contains(text(), 'Enroll in plan')])[1]";
 					WebElement enrollInPlan = driver.findElement(By.xpath(elementPath));
-					//	enrollInPlan.click();
+					System.out.println("MA and PDP plans are displayed:" +Expected_PlanType);
 					validateNew(enrollInPlan);
 					jsClickNew(enrollInPlan);
 				}
@@ -259,7 +259,7 @@ public class WelcomePage extends UhcDriver{
 					//String elementPath = "(//*[contains(text(), 'Enroll in plan')])[2]";
 					String elementPath ="(//*[contains(@data-ng-show, 'displayEnrollNow')])[2]";
 					WebElement enrollInPlan = driver.findElement(By.xpath(elementPath));
-					//	enrollInPlan.click();
+					System.out.println("SNP plans are displayed:" +Expected_PlanType);
 					validateNew(enrollInPlan);
 					jsClickNew(enrollInPlan);
 				}
